a28b4a758444b7debbb9b65737536686e29ba31d
Shelby Polasek <shelbypolasek@gmail.com>
2020-12-16 16:24:28 -0600

    rounded card nav

--- frontend/src/components/CardOverlay.js
+++ frontend/src/components/CardOverlay.js
@@ -4,7 +4,7 @@ import { Card, CardTitle, CardImg, CardImgOverlay } from 'reactstrap';
 const CardOverlay = ({imgSrc, cardTitle}) => {
     return (
             <Card className='border-0' inverse>
-                <CardImg className='w-100' src={imgSrc} alt="Card image cap" />
+                <CardImg className='w-100 rounded-circle' src={imgSrc} alt="Card image cap" />
                 <CardImgOverlay>
                     <CardTitle className='margin-auto' tag="h5">{cardTitle}</CardTitle>
                 </CardImgOverlay>
