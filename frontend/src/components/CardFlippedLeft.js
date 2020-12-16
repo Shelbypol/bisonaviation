bce2319abc783b5eb29abe142175d6533434613a
Shelby Polasek <shelbypolasek@gmail.com>
2020-12-16 14:08:09 -0600

    avionics desktop home rendered

--- frontend/src/components/CardFlippedLeft.js
+++ frontend/src/components/CardFlippedLeft.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import {
     Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button
+    CardTitle, CardSubtitle,
 } from 'reactstrap';
+import {Button, Row, Col} from "react-bootstrap";
 
-const CardFlippedLeft = ({ imgSrc, imgClassName, imgAlt ,title, titleStyle, subTitle, text, btnTitle, btnClassName }) => {
+const CardFlippedLeft = ({imgSrc, imgClassName, imgAlt, title, textIcon, btnIconVariant, btnBottomVariant, titleStyle, subTitle, text, btnIconStyle, btnBottomStyle, btnTitle}) => {
     return (
         <div className=''>
             <Card style={{zIndex: '1'}} className='d-inline-block w-100 border-0 cardStyle'>
-                <CardImg height='100%' width='25%' className={imgClassName} src={imgSrc} alt={imgAlt} />
-                <CardBody className='float-left textCard'>
+                <CardImg height='100%' width='25%' className={imgClassName} src={imgSrc} alt={imgAlt}/>
+                <CardBody className=' float-left textCard'>
                     <CardTitle tag="h4" className='font-weight-bold mb-4' style={{titleStyle}}>{title}</CardTitle>
                     <CardSubtitle tag="h6" className="text-muted mb-3">{subTitle}</CardSubtitle>
                     <hr/>
-                    <CardText className=''>{text}</CardText>
-                    <Button className={btnClassName}>{btnTitle}</Button>
+                    <Row>
+                        <Col xs={1}>
+                            <Button className='display-inline-block py-2 px-3 rounded-circle' style={{btnIconStyle}}
+                                    variant={btnIconVariant}>{textIcon}</Button>
+                        </Col>
+                        <Col xs={11}>
+                            <CardText className='display-inline-block ml-3' variant='danger'>{text}  </CardText>
+                        </Col>
+
+                    </Row>
                 </CardBody>
             </Card>
         </div>
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,15 +3,18 @@ import {Col, Container, Row} from 'react-bootstrap'
 import Jumbotrons from "../components/Jumbotrons";
 import CardFlippedLeft from "../components/CardFlippedLeft";
 import CardFlippedRight from "../components/CardFlippedRight"
+import AlertDismissible from "../components/AlertDismissible";
+import CardOverlay from "../components/CardImgOverlay";
 import aboutImg from '../images/jumbotron/hangar.png'
 import panel from '../images/activeImg/panel-divider.jpeg'
 import maintenance from '../images/activeImg/robin-cirrus-divider.png'
-import AlertDismissible from "../components/AlertDismissible";
-import CardOverlay from "../components/CardImgOverlay";
+import cncMachine from "../images/jumbotron/cnc-machine-divider.png"
+import tayseerAvionics from "../images/activeImg/tayseer-1.jpg"
+import panelCard from '../images/jumbotron/fullscreen-slider2.jpg'
 
 
 const HomeScreen = () => {
-    const [show, setShow] = useState(false);
+    // const [show, setShow] = useState(false);
 
     window.onbeforeunload = () => {
         localStorage.clear()
@@ -21,6 +24,7 @@ const HomeScreen = () => {
         <>
             <Jumbotrons imgClass={'heroJumbo '}/>
             <Jumbotrons imgClass={'fadeJumbo '}/>
+
 
             <Row className='' style={{marginTop: '-40vh', marginBottom: '20vh' }} xs={12}>
                 <Col className=''>
@@ -41,6 +45,9 @@ const HomeScreen = () => {
                     <CardFlippedLeft imgSrc={aboutImg}
                                      imgClassName={'img-fluid float-left imgCardAbout'}
                                      imgAlt={''}
+                                     btnIconStyle={{display: 'hidden'}}
+                                     btnIconVariant={'danger'}
+                                     textIcon={<i className="far fa-plane"> </i>}
                                      title={'Why choose Bison Aviation?'}
                                      titleStyle={{borderBottom: '3px solid darkred'}}
                                      subTitle={'AVIONICS & AIRCRAFT MAINTENANCE & FLIR'}
@@ -60,10 +67,45 @@ const HomeScreen = () => {
                              imgAlt={''}
                              title={'Avionics'}
                              subTitle={'who are we'}
-                             text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A autem consectetur deleniti dicta dignissimos eligendi laborum, maiores neque, nesciunt nisi non odit perferendis quasi quidem quos recusandae soluta veniam voluptatum.'}
+                             btnIconStyle={''}
+                             btnIconVariant={'danger'}
+                             textIcon={<i className="fal fa-tachometer-alt-slow "> </i>}
+                             text={'Bison Aviation is proud to have a full-time avionics team in-house. While many shops “sub out” their avionics work, Bison Aviation maintains a full service avionics staff as a part of our workforce. Our capabilities range from basic troubleshooting to complete panel installations and by having all services in-house (including CNC panel fabrication, laser engraving, and 3D printing) we are able to save time and reduce costs for our customers.'}
                              btnTitle={'Avionics'}
+                             btnBottomStyle={{visibility: 'hidden'}}
+                             btnBottomVariant={'none'}
             />
-            {/*<Jumbotrons imgClass={'fadeCardJumbo '}/>*/}
+
+            <CardFlippedRight imgSrc={tayseerAvionics}
+                             imgClassName={'img-fluid float-right imgCardAvionics'}
+                             titleStyle={''}
+                             imgAlt={''}
+                             title={''}
+                             subTitle={''}
+                             btnIconStyle={''}
+                             btnIconVariant={'danger'}
+                             textIcon={<i className="fal fa-tachometer-alt-slow "> </i>}
+                             text={'Whether you need to add ADS-B OUT capabilities in order to meet the December 31, 2019 deadline, a routine pitot-static / transponder certification, or it\'s time for a completely new panel, Bison Aviation\'s Avionics Team is ready to bring your avionics systems into the 21st century! We are installers for virtually all major avionics manufacturers, so we can build you the very best panel for your mission and budget. Cutting-edge technology, such as CNC machining, laser engraving, and 3D printing, put our installations in a class of their own!'}
+                             btnTitle={'Avionics'}
+                             btnBottomStyle={{visibility: 'hidden'}}
+                             btnBottomVariant={'none'}
+            />
+
+            <CardFlippedLeft imgSrc={cncMachine}
+                             imgClassName={'img-fluid float-left imgCardAvionics'}
+                             titleStyle={''}
+                             imgAlt={''}
+                             title={''}
+                             subTitle={''}
+                             btnIconStyle={''}
+                             btnIconVariant={'danger'}
+                             textIcon={<i className="fal fa-tachometer-alt-slow "> </i>}
+                             text={'When avionics systems stop working correctly, you need a team with the tools and experience necessary to efficiently diagnose the issues and repair the system. Whether the fault is a data bus communications error on a new glass panel PFD or a weak transmitter on a legacy com radio, Bison Aviation\'s skilled technicians can locate the fault and return those systems to operational condition quickly!'}
+                             btnTitle={'Avionics'}
+                             btnBottomStyle={{visibility: 'hidden'}}
+                             btnBottomVariant={'none'}
+            />
+
             <div className='fadeCardLeft'> </div>
 
 
@@ -71,17 +113,29 @@ const HomeScreen = () => {
 
 
 
-            <Row className='' style={{ marginBottom: '20vh' }} xs={12}>
-                <Col className=''>
-                    <CardOverlay />
-                </Col>
-                <Col className='m-auto'>
-                    <CardOverlay />
-                </Col>
-                <Col className='m-auto'>
-                    <CardOverlay />
-                </Col>
-            </Row>
+            {/*<Row className='' style={{ marginBottom: '30vh' }} xs={12}>*/}
+            {/*    <Col className=''>*/}
+            {/*        <CardOverlay imgSrc={cncMachine}*/}
+            {/*                     imgClassName={'overlayImg'}*/}
+            {/*                     cardTitle={''}*/}
+            {/*                     cardText={''}*/}
+            {/*                     cardSmallText={''} />*/}
+            {/*    </Col>*/}
+            {/*    <Col className='m-auto'>*/}
+            {/*        <CardOverlay imgSrc={tayseerAvionics}*/}
+            {/*                     imgClassName={'overlayImg'}*/}
+            {/*                     cardTitle={''}*/}
+            {/*                     cardText={''}*/}
+            {/*                     cardSmallText={''} />*/}
+            {/*    </Col>*/}
+            {/*    <Col className='m-auto'>*/}
+            {/*        <CardOverlay imgSrc={panelCard}*/}
+            {/*                     imgClassName={'overlayImg'}*/}
+            {/*                     cardTitle={''}*/}
+            {/*                     cardText={''}*/}
+            {/*                     cardSmallText={''} />*/}
+            {/*    </Col>*/}
+            {/*</Row>*/}
 
 
 
@@ -89,13 +143,7 @@ const HomeScreen = () => {
 
 
 
-            <CardFlippedRight imgSrc={maintenance}
-                              imgAlt={''}
-                              title={'Maintenance'}
-                              subTitle={'who are we'}
-                              text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A autem consectetur deleniti dicta dignissimos eligendi laborum, maiores neque, nesciunt nisi non odit perferendis quasi quidem quos recusandae soluta veniam voluptatum.'}
-                              btnTitle={'test button'}
-            />
+
             {/*<Jumbotrons imgClass={'fadeCardJumbo '}/>*/}
             <div className='fadeCardRight'> </div>
         </>
