585f0964f76255d11205c5bdac50cfb3af203766
Andrew Brought <andrewbrought@gmail.com>
2020-12-16 15:28:04 -0600

    pushing

--- frontend/src/components/AlertDismissible.js
+++ frontend/src/components/AlertDismissible.js
@@ -7,23 +7,26 @@ const AlertDismissible = (props) => {
 
     return (
         <>
-                <p>
+            {show &&
+
+            <Alert>
+                {/*<p>*/}
+                {props.title.toUpperCase()}
+                {props.title2 && ' ' + props.title2.toUpperCase()}
+                {': ' + props.note}
+                {/*</p>*/}
+            </Alert>
+
+
+            }
+                <p className='d-inline'>
+                    <Button onMouseOver={() => setShow(true)} onClick={() => setShow(false)} variant='danger' dismissible className='mr-2 circleHover' style={{borderRadius: "50%"}}>{props.icon}</Button>
                     <h5 className='mt-2'>{props.title}</h5>
-                    <Button onClick={() => setShow(true)} variant='danger' >{props.icon}</Button>
+                    <h5>{props.title2}</h5>
 
                 </p>
 
 
-            {show &&
-
-                <Alert onClose={() => setShow(false)} dismissible >
-                    <p>
-                        {props.note}
-                    </p>
-                </Alert>
-
-
-            }
         </>
     );
 }
