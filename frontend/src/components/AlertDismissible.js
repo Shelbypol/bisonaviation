198a5478a68b8484ac6501e133db0e42b823b3aa
Andrew Brought <andrewbrought@gmail.com>
2020-12-17 10:26:28 -0600

    got the closing action we want on each component

--- frontend/src/components/AlertDismissible.js
+++ frontend/src/components/AlertDismissible.js
@@ -7,24 +7,31 @@ const AlertDismissible = (props) => {
 
     return (
         <>
-            {show &&
 
-            <Alert>
-                {/*<p>*/}
-                {props.title.toUpperCase()}
-                {props.title2 && ' ' + props.title2.toUpperCase()}
-                {': ' + props.note}
-                {/*</p>*/}
-            </Alert>
-
-
-            }
                 <p className='d-inline'>
-                    <Button onMouseOver={() => setShow(true)} onClick={() => setShow(false)} variant='danger' dismissible className='mr-2 circleHover' style={{borderRadius: "50%"}}>{props.icon}</Button>
+                    <Button
+                        onMouseOver={() => setShow(true)}
+                        onClick={() => setShow(false)}
+                        variant='danger'
+                        dismissible className='mr-2 circleHover'
+                        style={{borderRadius: "50%"}}>{props.icon}
+                    </Button>
                     <h5 className='mt-2'>{props.title}</h5>
                     <h5>{props.title2}</h5>
 
                 </p>
+            {show &&
+
+            <Alert>
+                {/*<p>*/}
+                {/*{props.title.toUpperCase()}*/}
+                {/*{props.title2 && ' ' + props.title2.toUpperCase()}*/}
+                {/*<br/>*/}
+                {props.note}
+                {/*</p>*/}
+            </Alert>
+
+            }
 
 
         </>
