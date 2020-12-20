a8e45b668227aed376148a47f97936550ba34f1d
Shelby Polasek <shelbypolasek@gmail.com>
2020-12-20 13:50:40 -0600

    fully responsive header

--- frontend/src/components/DesktopStickyHeader.js
+++ frontend/src/components/DesktopStickyHeader.js
@@ -4,6 +4,8 @@ import {LinkContainer} from "react-router-bootstrap";
 import {useDispatch, useSelector} from "react-redux";
 import {CART_RESET} from "../constants/cartConstants";
 import {logout} from "../actions/userActions";
+import {Route} from "react-router-dom";
+import SearchBox from "./SearchBox";
 
 
 const DesktopStickyHeader = () => {
@@ -25,140 +27,153 @@ const DesktopStickyHeader = () => {
 
                 <Navbar className='navbar navbar-expand-lg navbar-light bg-light mx-auto my-0 p-0 border-0'>
 
-                    <Container className='col-sm-12 col-md-12'>
+                    <Container className='col-sm-12'>
 
                         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                         <Navbar.Collapse id="basic-navbar-nav">
 
-                            <Nav className='col-md-8 m-0 stickyNavBtn justify-content-end'>
 
-                                {/*  BROWSE PRODUCTS  */}
-                                <LinkContainer className='mx-1' to='/products'>
-                                    <Nav.Link>
-                                        Products
-                                    </Nav.Link>
-                                </LinkContainer>
+                                <Nav className='col-md-7 stickyNavBtn justify-content-md-end'>
+                                    {/*<Nav className='col-md-8 m-0 stickyNavBtn justify-content-center'>*/}
 
-                                {/*  AIR CRAFT SERVICES  */}
-                                <NavDropdown style={{zIndex: '100'}} className='mx-1' title='Aircraft Services'
-                                             id='aircraftservicesmenu'>
-                                    {/*   AVIONICS  */}
-                                    <LinkContainer to='/avionics'>
-                                        <NavDropdown.Item>Avionics</NavDropdown.Item>
+                                    {/*  BROWSE PRODUCTS  */}
+                                    <LinkContainer className='mx-1' to='/products'>
+                                        <Nav.Link>
+                                            Products
+                                        </Nav.Link>
                                     </LinkContainer>
 
-                                    {/*   MAINTENANCE  */}
-                                    <LinkContainer to='/maintenance'>
-                                        <NavDropdown.Item>Maintenance</NavDropdown.Item>
-                                    </LinkContainer>
-
-                                    {/*   AOG RESPONSE  */}
-                                    <LinkContainer to='/aog'>
-                                        <NavDropdown.Item>AOG Response</NavDropdown.Item>
-                                    </LinkContainer>
-
-                                    {/*   ACQUISITIONS  */}
-                                    <LinkContainer to='/acquisitions'>
-                                        <NavDropdown.Item>Acquisitions</NavDropdown.Item>
-                                    </LinkContainer>
-
-                                    {/*   AIRCRAFT PARTS  */}
-                                    <LinkContainer to='/aircraftparts'>
-                                        <NavDropdown.Item>Aircraft Parts</NavDropdown.Item>
-                                    </LinkContainer>
-                                </NavDropdown>
-
-                                {/*  EO/IR  */}
-                                <NavDropdown style={{zIndex: '100'}} className='mx-1' title='EO/IR' id='eoirmenu'>
-                                    {/*   EO / IR MAINTENANCE */}
-                                    <LinkContainer to='/eoir/maintenance'>
-                                        <NavDropdown.Item>EO/IR Maintenance</NavDropdown.Item>
-                                    </LinkContainer>
-
-                                    {/*   SYSTEM SALES  */}
-                                    <LinkContainer to='/systemsales'>
-                                        <NavDropdown.Item>System Sales</NavDropdown.Item>
-                                    </LinkContainer>
-
-                                    {/*   EO / IR PARTS  */}
-                                    <LinkContainer to='/eoir/parts'>
-                                        <NavDropdown.Item>EO/IR Parts</NavDropdown.Item>
-                                    </LinkContainer>
-                                </NavDropdown>
-
-                                {/*  TEAM  */}
-                                <LinkContainer to='/team'>
-                                    <Nav.Link>
-                                        Team
-                                    </Nav.Link>
-                                </LinkContainer>
-                            </Nav>
-
-                            {/*   PROFILE BTNS  */}
-                            <Nav className='col-md-4 justify-content-end'>
-
-                                {/*  PROFILE  /  SIGN IN  */}
-                                {userInfo ? (
-                                    <>
-                                        <LinkContainer to='/profile' className='pr-5'>
-                                            <Nav.Link>
-                                                Welcome, {userInfo.name}
-                                            </Nav.Link>
+                                    {/*  AIR CRAFT SERVICES  */}
+                                    <NavDropdown style={{zIndex: '100'}} className='mx-1' title='Aircraft Services'
+                                                 id='aircraftservicesmenu'>
+                                        {/*   AVIONICS  */}
+                                        <LinkContainer to='/avionics'>
+                                            <NavDropdown.Item>Avionics</NavDropdown.Item>
                                         </LinkContainer>
 
-                                        <LinkContainer to='/profile'>
-                                            <Nav.Link onClick={logoutHandler}>
-                                                {/*<Button onClick={logoutHandler} className='rounded-pill headerBtn'>*/}
-                                                <i style={{fontSize: '1.8em'}}
-                                                   className="pr-2 fal fa-plane-departure"> </i> Log out
-                                                {/*</Button>*/}
-                                            </Nav.Link>
+                                        {/*   MAINTENANCE  */}
+                                        <LinkContainer to='/maintenance'>
+                                            <NavDropdown.Item>Maintenance</NavDropdown.Item>
                                         </LinkContainer>
-                                    </>
 
-                                ) : <LinkContainer to='/login'>
-                                    <Nav.Link>
-                                        <Button className='px-3 py-1 rounded-pill bisonRedBtn iconHidden'>
+                                        {/*   AOG RESPONSE  */}
+                                        <LinkContainer to='/aog'>
+                                            <NavDropdown.Item>AOG Response</NavDropdown.Item>
+                                        </LinkContainer>
+
+                                        {/*   ACQUISITIONS  */}
+                                        <LinkContainer to='/acquisitions'>
+                                            <NavDropdown.Item>Acquisitions</NavDropdown.Item>
+                                        </LinkContainer>
+
+                                        {/*   AIRCRAFT PARTS  */}
+                                        <LinkContainer to='/aircraftparts'>
+                                            <NavDropdown.Item>Aircraft Parts</NavDropdown.Item>
+                                        </LinkContainer>
+                                    </NavDropdown>
+
+                                    {/*  EO/IR  */}
+                                    <NavDropdown style={{zIndex: '100'}} className='mx-1' title='EO/IR' id='eoirmenu'>
+                                        {/*   EO / IR MAINTENANCE */}
+                                        <LinkContainer to='/eoir/maintenance'>
+                                            <NavDropdown.Item>EO/IR Maintenance</NavDropdown.Item>
+                                        </LinkContainer>
+
+                                        {/*   SYSTEM SALES  */}
+                                        <LinkContainer to='/systemsales'>
+                                            <NavDropdown.Item>System Sales</NavDropdown.Item>
+                                        </LinkContainer>
+
+                                        {/*   EO / IR PARTS  */}
+                                        <LinkContainer to='/eoir/parts'>
+                                            <NavDropdown.Item>EO/IR Parts</NavDropdown.Item>
+                                        </LinkContainer>
+                                    </NavDropdown>
+
+                                    {/*  TEAM  */}
+                                    <LinkContainer to='/team'>
+                                        <Nav.Link>
+                                            Team
+                                        </Nav.Link>
+                                    </LinkContainer>
+                                </Nav>
+
+                            <Route render={({history}) => <SearchBox history={history}
+                                                                     searchSize={'sm'}
+                                                                     formClasses={'col-md-3 my-4'}
+                                                                     searchClasses={'col-10'}
+                                                                     btnSize={'sm'}
+                                                                     btnClasses={'bisonWhiteBtn px-1 col-2 rounded-right'}
+                                                                     iconStyle={{fontSize: '1.53em'}}
+                            />}/>
+
+
+                                {/*   PROFILE BTNS  */}
+                                <Nav className='col-md-2 justify-content-end'>
+
+                                    {/*  PROFILE  /  SIGN IN  */}
+                                    {userInfo ? (
+                                        <>
+                                            <LinkContainer to='/profile' className='pr-5'>
+                                                <Nav.Link>
+                                                    Welcome, {userInfo.name}
+                                                </Nav.Link>
+                                            </LinkContainer>
+
+                                            <LinkContainer to='/profile'>
+                                                <Nav.Link onClick={logoutHandler}>
+                                                    {/*<Button onClick={logoutHandler} className='rounded-pill headerBtn'>*/}
+                                                    <i style={{fontSize: '1.8em'}}
+                                                       className="pr-2 fal fa-plane-departure"> </i> Log out
+                                                    {/*</Button>*/}
+                                                </Nav.Link>
+                                            </LinkContainer>
+                                        </>
+
+                                    ) : <LinkContainer to='/login'>
+                                        <Nav.Link>
+                                            <Button className='px-3 py-1 rounded-pill bisonRedBtn iconHidden'>
                                      <span className='hideIcon'>
                                          <i style={{fontSize: '1.5em'}}
                                             className=" fal fa-plane-arrival "> </i>
                                      </span>
-                                            <span className='hideWords'>Login</span>
-                                        </Button>
-                                    </Nav.Link>
-                                </LinkContainer>
-                                }
-                                {/*  REGISTER  */}
-                                {!userInfo && (
-                                    <LinkContainer to='/Register'>
-                                        {/*<Nav.Link><i className='fas fa-user'> </i> Register*/}
-                                        <Nav.Link>
-                                            <Button className='px-3 py-1 rounded-pill bisonRedBtn '>
-                                               {/*<span className=''>*/}
-                                                 {/*<i style={{fontSize: '2em'}}*/}
-                                                 {/*   className="pr-2 fal fal fa-clipboard-user"> </i>*/}
-                                               {/*</span>*/}
-                                                <span className=''>Register</span>
+                                                <span className='hideWords'>Login</span>
                                             </Button>
                                         </Nav.Link>
-                                    </LinkContainer>)
-                                }
+                                    </LinkContainer>
+                                    }
+                                    {/*  REGISTER  */}
+                                    {!userInfo && (
+                                        <LinkContainer to='/Register'>
+                                            {/*<Nav.Link><i className='fas fa-user'> </i> Register*/}
+                                            <Nav.Link>
+                                                <Button className='px-3 py-1 rounded-pill bisonRedBtn '>
+                                                    {/*<span className=''>*/}
+                                                    {/*<i style={{fontSize: '2em'}}*/}
+                                                    {/*   className="pr-2 fal fal fa-clipboard-user"> </i>*/}
+                                                    {/*</span>*/}
+                                                    <span className=''>Register</span>
+                                                </Button>
+                                            </Nav.Link>
+                                        </LinkContainer>)
+                                    }
 
-                                {/*  ADMIN DROPDOWN  */}
-                                {userInfo && userInfo.isAdmin && (
-                                    <NavDropdown style={{zIndex: '100'}} title='Admin' id='adminmenu'>
-                                        <LinkContainer to='/admin/userlist'>
-                                            <NavDropdown.Item>Users</NavDropdown.Item>
-                                        </LinkContainer>
-                                        <LinkContainer to='/admin/productlist'>
-                                            <NavDropdown.Item>Products</NavDropdown.Item>
-                                        </LinkContainer>
-                                        <LinkContainer to='/admin/orderlist'>
-                                            <NavDropdown.Item>Orders</NavDropdown.Item>
-                                        </LinkContainer>
-                                    </NavDropdown>
-                                )}
-                            </Nav>
+                                    {/*  ADMIN DROPDOWN  */}
+                                    {userInfo && userInfo.isAdmin && (
+                                        <NavDropdown style={{zIndex: '100'}} title='Admin' id='adminmenu'>
+                                            <LinkContainer to='/admin/userlist'>
+                                                <NavDropdown.Item>Users</NavDropdown.Item>
+                                            </LinkContainer>
+                                            <LinkContainer to='/admin/productlist'>
+                                                <NavDropdown.Item>Products</NavDropdown.Item>
+                                            </LinkContainer>
+                                            <LinkContainer to='/admin/orderlist'>
+                                                <NavDropdown.Item>Orders</NavDropdown.Item>
+                                            </LinkContainer>
+                                        </NavDropdown>
+                                    )}
+                                </Nav>
+
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
