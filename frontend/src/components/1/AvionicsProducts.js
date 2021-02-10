8641f4f9ecabb407cb6f768cb2cd8c0b355fac53
Shelby Polasek <shelbypolasek@gmail.com>
2021-02-10 16:48:44 -0600

    garmin section added

--- frontend/src/components/1/AvionicsProducts.js
+++ frontend/src/components/1/AvionicsProducts.js
@@ -1,30 +1,38 @@
 import React, {useState} from 'react';
-import {Col, Container, Row} from "react-bootstrap";
+import {Col, Button, Row} from "react-bootstrap";
 import {Jumbotron} from 'reactstrap';
-import '../../style/1/AvionicsProducts.css'
 import Fade from 'react-reveal/Zoom';
+import '../../style/1/AvionicsProducts.css'
+import '../../style/1/AvionicsProductsFlag.scss'
 
 const AvionicsProducts = () => {
 
 
         return (
-            <Jumbotron className='global_cultured mt-4 mx-0 mb-0 p-0 AvionicsProducts_jumbo' fluid>
+            <Jumbotron className='global_cultured mt-2 mx-0 mb-0 p-0 AvionicsProducts_jumbo' fluid>
 
                 <Row>
-                    <Col xs={6}>
-                        <Fade duration={3000} left>
-                            <div className='AvionicsProducts_jumbo-1 global_eerie-black-bg'>
-                                <h1>from left</h1>
+                    <Col xs={8}>
+                        <Fade duration={1500} left>
+                            <div className='AvionicsProducts_jumbo AvionicsProducts_img-left global_eerie-black-bg'>
+                                <p className='Avionics_flag red global_accentFont mt-n1 mb-0 mx-0'>NEW!</p>
+                                {/*<h1 className='text-center global_cultured AvionicProducts_jumbo-left'>Certified Garmin Dealers</h1>*/}
                             </div>
                         </Fade>
                     </Col>
-                    <Col xs={6}>
-                        <Fade duration={3000} right>
-                            <div className='AvionicsProducts_jumbo-1 global_blood-red-bg'>
-                                <h1>from left</h1>
+                    <Col xs={4}>
+                        <Fade duration={1500} right>
+                            <div className='AvionicsProducts_jumbo global_cultured d-flex justify-content-center align-items-center'>
+                                <Button className='AvionicsProducts_searchBtn'>Browse Products</Button>
                             </div>
                         </Fade>
                     </Col>
+                    {/*<Col xs={6}>*/}
+                    {/*    <Fade duration={2000} right>*/}
+                    {/*        <div className='AvionicsProducts_jumbo AvionicsProducts_img-right global_blood-red-bg'>*/}
+                    {/*        </div>*/}
+                    {/*    </Fade>*/}
+                    {/*</Col>*/}
 
                 </Row>
 
