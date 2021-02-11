215e6d520ca8bef85254200c37ea9fea30eb99e8
Shelby Polasek <shelbypolasek@gmail.com>
2021-02-11 13:03:15 -0600

    services sec aligned

--- frontend/src/components/1/Services.js
+++ frontend/src/components/1/Services.js
@@ -8,49 +8,46 @@ const Services = ({title1, title2, title3, title4, showTag}) => {
     return (
 
         <Jumbotron className='m-0 p-0 bg-white Services_NavBg global_silver-chalice-bg d-flex align-content-stretch'
-                   fluid>
-            {/*<Row xs={12} className=' m-auto'>*/}
+            fluid>
 
-                <Col xs={6} className='border d-flex align-items-center'>
+                <Col xs={6} className='border d-flex align-items-center justify-content-center'>
                     <h1 className='global_title'>Services</h1>
                 </Col>
 
-                <Row xs={6} className='border d-flex align-items-center align-content-center mx-auto'>
-                    <Col xs={12} className='mb-3 Services_nav_hoverTitle'>
+                <Row xs={6} className='border d-flex align-items-end align-content-center'>
+                    <Col xs={12} className='mb-3 Services_nav_hoverTitle d-flex justify-content-center'>
                         <Button className='Services_nav_overlay'>
                             {showTag ?
                                 <>
                                     <p className='flag red global_accentFont mt-n1 mb-0 mx-0'>NEW!</p>
-                                    <p className=' Services_nav_text global_accentFont'>{title1}</p>
+                                    <p className='Services_nav_text global_accentFont'>{title1}</p>
                                 </>
                                 : (
-                                    <p className=' Services_nav_text global_accentFont'>{title1}</p>
+                                    <p className='Services_nav_text global_accentFont'>{title1}</p>
                                 )
                             }
                         </Button>
                     </Col>
 
-                    <Col xs={12} className=' mb-3 Services_nav_hoverTitle'>
+                    <Col xs={12} className='Services_nav_hoverTitle mb-3 d-flex justify-content-center'>
                         <Button className='Services_nav_overlay'>
                             <p className='Services_nav_text global_accentFont'>{title2}</p>
                         </Button>
                     </Col>
 
-                    <Col xs={12} className='Services_nav_hoverTitle mb-3'>
+                    <Col xs={12} className='Services_nav_hoverTitle mb-3 d-flex justify-content-center'>
                         <Button className='Services_nav_overlay '>
                             <p className='Services_nav_text global_accentFont'>{title3}</p>
                         </Button>
                     </Col>
 
-                    <Col xs={12} className='Services_nav_hoverTitle mb-3'>
+                    <Col xs={12} className='Services_nav_hoverTitle mb-3 d-flex justify-content-center'>
                         <Button className='Services_nav_overlay'>
                             <p className='Services_nav_text global_accentFont'>{title4}</p>
                         </Button>
                     </Col>
 
                 </Row>
-
-            {/*</Row>*/}
 
         </Jumbotron>
 
