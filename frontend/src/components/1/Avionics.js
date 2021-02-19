176de713d10361c34539f0a2f3ee5e3ba217eda0
Shelby Polasek <shelbypolasek@gmail.com>
2021-02-19 16:15:47 -0600

    avionics section updated

--- frontend/src/components/1/Avionics.js
+++ frontend/src/components/1/Avionics.js
@@ -38,88 +38,77 @@ const Avionics = () => {
 
 
                     <Col xs={6} className=''>
-                        <Row className='mb-5'>
-                            <Col xs={12}>
-                                <AvionicsProducts/>
 
+                        <Row className=' mx-3 text-center'>
+                            <h4 className='global_eerie-black'>
+                                {/*FROM MINOR REPAIRS TO COMPLETE PANEL UPGRADES. ALL PERFORMED IN-HOUSE AT BISON AVIATION.*/}
+                                From minor Repairs to complete panel updgrades. ALL SERVICES PERFROMED IN-HOUSE AT BISON
+                                AVIATION.
+                            </h4>
+                        </Row>
+
+                        <Row className='mb-3'>
+                            <Col xs={12}>
+                                <hr/>
+                                <AvionicsProducts/>
+                                <hr/>
                             </Col>
                         </Row>
 
-
-                        <Row className='mb-3 '>
+                        <Row className=' Avionics_info'>
                             <Zoom>
-                                <Col xs={2} className='d-flex justify-content-center mt-3'>
-                                    <i className='far fa-tablet-rugged global_blood-red'
-                                       style={{fontSize: '3em'}}> </i>
+                                <Col xs={1} className='d-flex justify-content-center mt-3'>
+                                    <i className='far fa-tablet-alt global_blood-red'
+                                       style={{fontSize: '2em'}}> </i>
                                 </Col>
-                                <Col xs={10} className='py-3'>
-                                    <h4 className='global_eerie-black'>Full / Partial Panel Upgrades</h4>
+                                <Col xs={12} className=''>
+                                    <h3 className='global_eerie-black'>Full / Partial Panel Upgrades</h3>
                                 </Col>
                             </Zoom>
-                                {/*<Row xs={12}>*/}
-                                {/*    <Col xs={2} className='d-flex justify-content-center mt-3'>*/}
-                                {/*        <i className='far fa-tablet-rugged global_blood-red'*/}
-                                {/*           style={{fontSize: '3em'}}> </i>*/}
-                                {/*    </Col>*/}
-                                {/*    <Col xs={8}>*/}
-                                {/*        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis,*/}
-                                {/*            repellat.</p>*/}
-                                {/*    </Col>*/}
-                                {/*</Row>*/}
-                            {/*</Col>*/}
+
                         </Row>
 
 
-                        <Row className='mb-3'>
-                            {/*<Col xs={12}>*/}
-                                <Zoom>
-                                    <Col xs={2} className='d-flex justify-content-center mt-3'>
-                                        <i className='far fa-tablet-rugged global_blood-red'
-                                           style={{fontSize: '3em'}}> </i>
-                                    </Col>
-                                    <Col xs={10} className='py-3'>
-                                        <h4 className='global_eerie-black'>Test, Checks & Repairs</h4>
-                                    </Col>
-                                </Zoom>
-                                {/*<Row xs={12}>*/}
-                                {/*    <Col xs={2} className='d-flex justify-content-center mt-3'>*/}
-                                {/*        <i className='far fa-tablet-rugged global_blood-red'*/}
-                                {/*           style={{fontSize: '3em'}}> </i>*/}
-                                {/*    </Col>*/}
-                                {/*    <Col xs={8}>*/}
-                                {/*        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis,*/}
-                                {/*            repellat.</p>*/}
-                                {/*    </Col>*/}
-                                {/*</Row>*/}
-                            {/*</Col>*/}
+                        <Row className='Avionics_info'>
+                            <Zoom>
+                                <Col xs={1} className='d-flex justify-content-center mt-3'>
+                                    <i className='fal fa-tachometer-alt-fastest global_blood-red'
+                                       style={{fontSize: '2em'}}> </i>
+                                </Col>
+                                <Col xs={12} className=''>
+                                    <h3 className='global_eerie-black'>In-depth Tests & Checks</h3>
+                                </Col>
+                            </Zoom>
+
                         </Row>
 
 
-                        <Row className='mb-3'>
-                            {/*<Col xs={12}>*/}
-                                {/*<Col xs={12}>*/}
-                                    <Zoom>
-                                        <Col xs={2} className='d-flex justify-content-center mt-3'>
-                                            <i className='far fa-tablet-rugged global_blood-red'
-                                               style={{fontSize: '3em'}}> </i>
-                                        </Col>
-                                        <Col xs={10} className='py-3'>
-                                            <h4 className='global_eerie-black'>Software Updates</h4>
-                                        </Col>
-                                    </Zoom>
+                        <Row className='Avionics_info'>
+                            <Zoom>
+                                <Col xs={1} className='d-flex justify-content-center mt-3'>
+                                    <i className='fal fa-database global_blood-red'
+                                       style={{fontSize: '2em'}}> </i>
+                                </Col>
+                                <Col xs={12} className=''>
+                                    <h3 className='global_eerie-black'>Software Updates</h3>
+                                </Col>
+                            </Zoom>
+                        </Row>
+
+                        <Row className='mt-3 ml-5 Avionics_info'>
+                            <Zoom>
+                                {/*<Col xs={1} className='d-flex justify-content-center mt-2'>*/}
+                                {/*    <i className='far fa-tablet-rugged global_blood-red'*/}
+                                {/*       style={{fontSize: '2em'}}> </i>*/}
                                 {/*</Col>*/}
-                                {/*<Row xs={12}>*/}
-                                {/*    <Col xs={2} className='d-flex justify-content-center mt-3'>*/}
-                                {/*        <i className='far fa-tablet-rugged global_blood-red'*/}
-                                {/*           style={{fontSize: '3em'}}> </i>*/}
-                                {/*    </Col>*/}
-                                {/*    <Col xs={8}>*/}
-                                {/*        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis,*/}
-                                {/*            repellat.</p>*/}
-                                {/*    </Col>*/}
-                                {/*</Row>*/}
-
-                            {/*</Col>*/}
+                                <Col xs={12} className='py-1 global_cursor'>
+                                    <Fade right duration='1000'>
+                                        <LinkContainer to={`/avionics`}>
+                                            <h4 className='global_blood-red'>Learn More</h4>
+                                        </LinkContainer>
+                                    </Fade>
+                                </Col>
+                            </Zoom>
                         </Row>
 
 
@@ -193,7 +182,7 @@ const Avionics = () => {
                 <Col xs={12} className='d-flex justify-content-end align-items-center'>
                     <LinkContainer to={`/contact`}>
                         <Fade right duration='1000'>
-                            <Button className=' Avionics_contact-btn'>Book an appointment
+                            <Button className=' Avionics_contact-btn'>Book your appointment
                                 today
                             </Button>
                         </Fade>
