caedc3ca9a377cb8965505a1ab12dfb9fbfdb977
Shelby Polasek <shelbypolasek@gmail.com>
2021-03-04 12:43:32 -0600

    team card set up

--- frontend/src/components/2/team-screen/TeamCard.js
+++ frontend/src/components/2/team-screen/TeamCard.js
@@ -18,12 +18,49 @@ const TeamCard = (props) => {
                     <Accordion>
                         <Card>
                             <Card.Header>
+                                <Card.Img variant="top" src="holder.js/100px180" />
                                 <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                                    Click me!
+                                    John D. Atkin IV, ATP / MEII
                                 </Accordion.Toggle>
                             </Card.Header>
                             <Accordion.Collapse eventKey="0">
-                                <Card.Body>Hello! I'm the body</Card.Body>
+
+                                <Card.Body>
+                                    <Jumbotron className='mx-auto bg-transparent border'
+                                               fluid>
+                                        <Row>
+                                            <Col xs={6}>
+                                                <Card.Img variant="top" src="holder.js/100px180" />
+                                            </Col>
+                                            <Col xs={12}>
+                                                {/*MEMBER NAME*/}
+                                                John D. Atkin IV, ATP / MEII
+                                            </Col>
+                                            <Col xs={12}>
+                                                {/*POSITION*/}
+                                                President
+                                            </Col>
+                                            <Col xs={12}>
+                                                {/*CERTIFICATIONS*/}
+                                                President
+                                            </Col>
+                                                <Col xs={12}>
+                                                {/*CERTIFICATIONS*/}
+                                                President
+                                            </Col>
+
+                                        </Row>
+
+                                    </Jumbotron>
+
+
+
+
+                                </Card.Body>
+
+
+
+
                             </Accordion.Collapse>
                         </Card>
                     </Accordion>
