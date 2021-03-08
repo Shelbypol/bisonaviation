ff747d6619107cff7cf1912fdcdb4a263e4935e8
Shelby Polasek <shelbypolasek@gmail.com>
2021-03-08 16:45:33 -0600

    maintenance screen carousel updated

--- frontend/src/screens/MaintenanceScreen.js
+++ frontend/src/screens/MaintenanceScreen.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import {Container} from "react-bootstrap";
 import MechanicsHero from "../components/2/mechanics-screen/MechanicsHero";
+import {Jumbotron} from "reactstrap";
 
 
 const MaintenanceScreen = () => {
     return (
         <>
-           <MechanicsHero/>
+            <Jumbotron className='MechanicsScreen_dots bg-transparent mb-0 pb-0' fluid>
+                <MechanicsHero/>
+            </Jumbotron>
         </>
     )
 };
