7c7f98780501d93d53deb11266cdea1e9cd985bc
Shelby Polasek <shelbypolasek@gmail.com>
2021-03-11 12:30:02 -0600

    mask image added to hero carousel

--- frontend/src/components/2/mechanics-screen/MechanicsHero.js
+++ frontend/src/components/2/mechanics-screen/MechanicsHero.js
@@ -4,7 +4,6 @@ import '../../../style/animations.css'
 import {Row, Col, Jumbotron} from 'reactstrap';
 import Fade from 'react-reveal/Fade';
 import Zoom from 'react-reveal/Zoom';
-import Spin from 'react-reveal/Spin';
 import img1 from '../../../images/bg-graphics/screen-maintenance/maintenance1.jpg'
 import img2 from '../../../images/bg-graphics/screen-maintenance/maintenance2.png'
 import img4 from '../../../images/bg-graphics/screen-maintenance/maintenance4.png'
@@ -15,14 +14,13 @@ import {Carousel} from "react-bootstrap"
 const MechanicsHero = () => {
     return (
         <>
-            <Carousel style={{height: '350px', overflow: 'hidden'}}>
-                <Carousel.Item className=' border-bottom border-5 border-dark'>
-
+            <Carousel style={{height: '350px', overflow: 'hidden'}} className=''>
+                <Carousel.Item className='bg-transparent border-bottom border-5 border-dark '>
                     <Carousel.Caption>
                         <Row className='MechanicsHero_text-box'>
                             <Zoom>
                                 <Col xs={6}
-                                     className=' d-flex justify-content-start align-content-center bg-transparent'>
+                                     className=' d-flex justify-content-start align-content-center '>
                                     <h1 className='MechanicsHero_title text-center ml-n2 global_cultured'>
                                         <span className='MechanicsHero_letter global_cultured'>M</span>aintenance
                                     </h1>
@@ -32,23 +30,19 @@ const MechanicsHero = () => {
                                 <h3 className='text-center m-0 p-0 global_cultured'>we work hard on the ground</h3>
                             </Col>
                             <Col xs={12} className='d-flex justify-content-start'>
-                                <h3 className='text-center m-0 p-0 global_cultured'>so you can
+                                <h3 className='text-center m-0 p-0 global_cultured'>so you can fly with confidence
                                     <Fade duration={5000}>
                                         <div className='d-inline'>
-                                <span className='global_silver-chalice font-weight-bolder MechanicsHero_span'>&nbsp;
-                                    <i>fly with confidence</i>
-                                </span>
                                         </div>
                                     </Fade>
                                 </h3>
                             </Col>
                         </Row>
                     </Carousel.Caption>
-                    {/*</Jumbotron>*/}
 
-                    <div className='animations_img-hover-zoom-in'>
+                    <div className='animations_img-hover-zoom-in MechanicsScreen_dots'>
                             <img
-                                className=" MechanicsHero_img "
+                                className=' '
                                 src={img1}
                                 alt="First slide"
                                 height={'100%'}
