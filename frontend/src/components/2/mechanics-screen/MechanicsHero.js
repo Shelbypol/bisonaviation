627f1f202f627866c07d1ee4fde425f1454b5563
Shelby Polasek <shelbypolasek@gmail.com>
2021-03-12 16:04:59 -0600

    mobile responsive hero carousel text box

--- frontend/src/components/2/mechanics-screen/MechanicsHero.js
+++ frontend/src/components/2/mechanics-screen/MechanicsHero.js
@@ -1,64 +1,47 @@
 import React from 'react'
 import '../../../style/mechanicsScreen/MechanicsHero.css'
-import '../../../style/animations.css'
-import {Row, Col} from 'reactstrap';
-import Slide from 'react-reveal/Slide';
-import img1 from '../../../images/bg-graphics/fs-slider-caption-bg.png'
-// import img1 from '../../../images/bg-graphics/screen-maintenance/maintenance1.jpg'
-import img2 from '../../../images/bg-graphics/screen-maintenance/maintenance2.png'
-import img4 from '../../../images/bg-graphics/screen-maintenance/maintenance4.png'
-import img5 from '../../../images/bg-graphics/screen-maintenance/maintenance5.png'
-import dots from '../../../images/bg-graphics/fs-slider-dots3.png'
-import {Carousel} from "react-bootstrap"
+import HeroCarousel from "../../HeroCarousel";
 
 
 const MechanicsHero = () => {
     return (
         <>
-            {/*<Carousel style={{height: '1500px', overflow: 'hidden'}} className=''>*/}
-            <Carousel>
-                <Carousel.Item className='border-bottom border-5 border-dark'>
-                    <Carousel.Caption>
-                        <Row className='global_carousel-text-box'>
-                            <Col md={5} xs={7} className='global_carousel-text-top-bar global_blood-red-bg'> </Col>
+            <HeroCarousel
+                slide1firstTitleLetter={'M'}
+                slide1firstTitleWord={'aintenance'}
+                slide1firstSentence={'Looks complicated...'}
+                slide1secondSentenceTrue={true}
+                slide1secondSentence={'...that\'s because it is!'}
 
-                            <Col xs={12}
-                                 className='d-flex justify-content-start align-content-center '>
-                                <h1 className='global_carousel-title ml-n2 global_cultured'>
-                                    <span className='global_carousel-letter global_cultured'>M</span>aintenance
-                                </h1>
-                            </Col>
+                carouselImg2True={true}
+                slide2firstTitleLetter={'F'}
+                slide2firstTitleWord={'rom gliders...'}
+                slide2firstSentence={'...to gulfstreams, THE BISON AVIATION '}
+                slide2secondSentenceTrue={true}
+                slide2secondSentence={'TEAM KNOWS YOUR AIRCRAFT INSIDE AND OUT!'}
 
-                            <Col xs={12} className=' d-flex justify-content-start'>
-                                <h3 className='global_carousel-text m-0 p-0 global_cultured'>we work hard on the
-                                    ground</h3>
-                            </Col>
+                carouselImg3True={false}
+                // slide3firstTitleLetter={}
+                // slide3firstTitleWord={}
+                // slide3firstSentence={}
+                // slide3secondSentenceTrue={}
+                // slide3secondSentence={}
 
-                            <Col xs={12} className='d-flex justify-content-start'>
-                                <h3 className='global_carousel-text m-0 p-0 global_cultured'>so you can fly with
-                                    confidence
-                                </h3>
-                            </Col>
+                carouselImg4True={false}
+                // slide4firstTitleLetter={}
+                // slide4firstTitleWord={}
+                // slide4firstSentence={}
+                // slide4secondSentenceTrue={}
+                // slide4secondSentence={}
 
-                            <Col md={7} xs={10} className='global_carousel-text-btm-bar global_blood-red-bg'> </Col>
-                        </Row>
-                    </Carousel.Caption>
+                carouselImg5True={false}
+                // // slide5firstTitleLetter={}
+                // // slide5firstTitleWord={}
+                // // slide5firstSentence={}
+                // // slide5secondSentenceTrue={}
+                // // slide5secondSentence={}
+            />
 
-                        <div className=' global_carousel-bg'>
-                    <Slide left duration={2500}>
-                            <img
-                                className='global_carousel-divider-img '
-                                src={img1}
-                                alt="First slide"
-                                height={'100%'}
-                            />
-                    </Slide>
-                        </div>
-
-                </Carousel.Item>
-
-
-            </Carousel>
         </>
     );
 };
--- frontend/src/components/HeroCarousel.js
+++ frontend/src/components/HeroCarousel.js
@@ -1,26 +1,45 @@
 import React from 'react'
 import {Row, Col} from 'reactstrap';
 import img1 from '../images/bg-graphics/fs-slider-caption-bg.png'
-// import img1 from '../../../images/bg-graphics/screen-maintenance/maintenance1.jpg'
-import img2 from '../images/bg-graphics/screen-maintenance/maintenance2.png'
-import img4 from '../images/bg-graphics/screen-maintenance/maintenance4.png'
-import img5 from '../images/bg-graphics/screen-maintenance/maintenance5.png'
-import dots from '../images/bg-graphics/fs-slider-dots3.png'
 import {Carousel} from "react-bootstrap"
 import Slide from "react-reveal/Slide";
 
 
 const HeroCarousel = (
     {
-        firstTitleLetter,
-        firstTitleWord,
-        firstSentence,
-        secondSentence,
-        secondSentenceTrue,
+        slide1firstTitleLetter,
+        slide1firstTitleWord,
+        slide1firstSentence,
+        slide1secondSentenceTrue,
+        slide1secondSentence,
+
         carouselImg2True,
+        slide2firstTitleLetter,
+        slide2firstTitleWord,
+        slide2firstSentence,
+        slide2secondSentenceTrue,
+        slide2secondSentence,
+
         carouselImg3True,
+        slide3firstTitleLetter,
+        slide3firstTitleWord,
+        slide3firstSentence,
+        slide3secondSentenceTrue,
+        slide3secondSentence,
+
         carouselImg4True,
-        carouselImg5True
+        slide4firstTitleLetter,
+        slide4firstTitleWord,
+        slide4firstSentence,
+        slide4secondSentenceTrue,
+        slide4secondSentence,
+
+        carouselImg5True,
+        slide5firstTitleLetter,
+        slide5firstTitleWord,
+        slide5firstSentence,
+        slide5secondSentenceTrue,
+        slide5secondSentence,
 
     }
 ) => {
@@ -38,17 +57,17 @@ const HeroCarousel = (
                                  className='d-flex justify-content-start align-content-center '>
                                 <h1 className='global_carousel-title ml-n2 global_cultured'>
                                     <span
-                                        className='global_carousel-letter global_cultured'>{firstTitleLetter}</span>{firstTitleWord}
+                                        className='global_carousel-letter global_cultured'>{slide1firstTitleLetter}</span>{slide1firstTitleWord}
                                 </h1>
                             </Col>
 
                             <Col xs={12} className=' d-flex justify-content-start'>
-                                <h3 className='global_carousel-text m-0 p-0 global_cultured'>{firstSentence}</h3>
+                                <h3 className='global_carousel-text m-0 p-0 global_cultured'>{slide1firstSentence}</h3>
                             </Col>
 
-                            {secondSentenceTrue && (
+                            {slide1secondSentenceTrue && (
                                 <Col xs={12} className='d-flex justify-content-start'>
-                                    <h3 className='global_carousel-text m-0 p-0 global_cultured'>{secondSentence}</h3>
+                                    <h3 className='text-left global_carousel-text m-0 p-0 global_cultured'>{slide1secondSentence}</h3>
                                 </Col>
                             )}
 
@@ -67,7 +86,7 @@ const HeroCarousel = (
                         </Slide>
                     </div>
                 </Carousel.Item>
-
+                slide2
                 {/*    ================= Img 2 */}
 
                 {carouselImg2True && (
@@ -80,17 +99,19 @@ const HeroCarousel = (
                                      className='d-flex justify-content-start align-content-center '>
                                     <h1 className='global_carousel-title ml-n2 global_cultured'>
                                     <span
-                                        className='global_carousel-letter global_cultured'>{firstTitleLetter}</span>{firstTitleWord}
+                                        className='global_carousel-letter global_cultured'>{slide2firstTitleLetter}</span>{slide2firstTitleWord}
                                     </h1>
                                 </Col>
 
                                 <Col xs={12} className=' d-flex justify-content-start'>
-                                    <h3 className='global_carousel-text m-0 p-0 global_cultured'>{firstSentence}</h3>
+                                    <h3 className='global_carousel-text m-0 p-0 global_cultured'>{slide2firstSentence}</h3>
                                 </Col>
 
-                                <Col xs={12} className='d-flex justify-content-start'>
-                                    <h3 className='global_carousel-text m-0 p-0 global_cultured'>{secondSentence}</h3>
-                                </Col>
+                                {slide2secondSentenceTrue && (
+                                    <Col xs={12} className='d-flex justify-content-start'>
+                                        <h3 className='global_carousel-text m-0 p-0 global_cultured'>{slide2secondSentence}</h3>
+                                    </Col>
+                                )}
 
                                 <Col md={7} xs={10} className='global_carousel-text-btm-bar global_blood-red-bg'> </Col>
                             </Row>
@@ -121,17 +142,19 @@ const HeroCarousel = (
                                      className='d-flex justify-content-start align-content-center '>
                                     <h1 className='global_carousel-title ml-n2 global_cultured'>
                                     <span
-                                        className='global_carousel-letter global_cultured'>{firstTitleLetter}</span>{firstTitleWord}
+                                        className='global_carousel-letter global_cultured'>{slide3firstTitleLetter}</span>{slide3firstTitleWord}
                                     </h1>
                                 </Col>
 
                                 <Col xs={12} className=' d-flex justify-content-start'>
-                                    <h3 className='global_carousel-text m-0 p-0 global_cultured'>{firstSentence}</h3>
+                                    <h3 className='global_carousel-text m-0 p-0 global_cultured'>{slide3firstSentence}</h3>
                                 </Col>
 
-                                <Col xs={12} className='d-flex justify-content-start'>
-                                    <h3 className='global_carousel-text m-0 p-0 global_cultured'>{secondSentence}</h3>
-                                </Col>
+                                {slide3secondSentenceTrue && (
+                                    <Col xs={12} className='d-flex justify-content-start'>
+                                        <h3 className='global_carousel-text m-0 p-0 global_cultured'>{slide3secondSentence}</h3>
+                                    </Col>
+                                )}
 
                                 <Col md={7} xs={10} className='global_carousel-text-btm-bar global_blood-red-bg'> </Col>
                             </Row>
@@ -164,17 +187,19 @@ const HeroCarousel = (
                                      className='d-flex justify-content-start align-content-center '>
                                     <h1 className='global_carousel-title ml-n2 global_cultured'>
                                     <span
-                                        className='global_carousel-letter global_cultured'>{firstTitleLetter}</span>{firstTitleWord}
+                                        className='global_carousel-letter global_cultured'>{slide4firstTitleLetter}</span>{slide4firstTitleWord}
                                     </h1>
                                 </Col>
 
                                 <Col xs={12} className=' d-flex justify-content-start'>
-                                    <h3 className='global_carousel-text m-0 p-0 global_cultured'>{firstSentence}</h3>
+                                    <h3 className='global_carousel-text m-0 p-0 global_cultured'>{slide4firstSentence}</h3>
                                 </Col>
 
-                                <Col xs={12} className='d-flex justify-content-start'>
-                                    <h3 className='global_carousel-text m-0 p-0 global_cultured'>{secondSentence}</h3>
-                                </Col>
+                                {slide4secondSentenceTrue && (
+                                    <Col xs={12} className='d-flex justify-content-start'>
+                                        <h3 className='global_carousel-text m-0 p-0 global_cultured'>{slide4secondSentence}</h3>
+                                    </Col>
+                                )}
 
                                 <Col md={7} xs={10} className='global_carousel-text-btm-bar global_blood-red-bg'> </Col>
                             </Row>
@@ -205,17 +230,19 @@ const HeroCarousel = (
                                      className='d-flex justify-content-start align-content-center '>
                                     <h1 className='global_carousel-title ml-n2 global_cultured'>
                                     <span
-                                        className='global_carousel-letter global_cultured'>{firstTitleLetter}</span>{firstTitleWord}
+                                        className='global_carousel-letter global_cultured'>{slide5firstTitleLetter}</span>{slide5firstTitleWord}
                                     </h1>
                                 </Col>
 
                                 <Col xs={12} className=' d-flex justify-content-start'>
-                                    <h3 className='global_carousel-text m-0 p-0 global_cultured'>{firstSentence}</h3>
+                                    <h3 className='global_carousel-text m-0 p-0 global_cultured'>{slide5firstSentence}</h3>
                                 </Col>
 
-                                <Col xs={12} className='d-flex justify-content-start'>
-                                    <h3 className='global_carousel-text m-0 p-0 global_cultured'>{secondSentence}</h3>
-                                </Col>
+                                {slide5secondSentenceTrue && (
+                                    <Col xs={12} className='d-flex justify-content-start'>
+                                        <h3 className='global_carousel-text m-0 p-0 global_cultured'>{slide5secondSentence}</h3>
+                                    </Col>
+                                )}
 
                                 <Col md={7} xs={10} className='global_carousel-text-btm-bar global_blood-red-bg'> </Col>
                             </Row>
