2dacca2281af09ed955ec5e4f0ca263ef197285f
Shelby Polasek <shelbypolasek@gmail.com>
2021-03-15 15:46:31 -0500

    flir table 1/4 info entered

--- frontend/src/components/2/flir-screen/FlirTable.js
+++ frontend/src/components/2/flir-screen/FlirTable.js
@@ -32,32 +32,537 @@ const FlirTable = () => {
             </tr>
             <tr>
                 <td>
-
+                    FLIR Systems, Inc.
                 </td>
                 <td>
-
+                    Model 2000A/B - Complete System
                 </td>
                 <td>
-
+                    FLIR 2000A/B
                 </td>
                 <td>
-
                 </td>
             </tr>
             <tr>
                 <td>
-
+                    FLIR Systems, Inc.
                 </td>
                 <td>
-
+                    Model 2000B - Complete System
                 </td>
                 <td>
-
+                    FLIR 2000B
                 </td>
                 <td>
-
                 </td>
             </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Model 2000F - Complete System
+                </td>
+                <td>
+                    FLIR 2000F
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Model 2000F/V - Complete System
+                </td>
+                <td>
+                    FLIR 2000F/V
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Model 2000F/V II - Complete System
+                </td>
+                <td>
+                    FLIR 2000F/V II
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Model 2000G - Complete System
+                </td>
+                <td>
+                    FLIR 2000G
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Model 2000G/V - Complete System
+                </td>
+                <td>
+                    FLIR 2000G/V
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Model 2000HP - Complete System
+                </td>
+                <td>
+                    FLIR 2000HP
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Model SAFIRE CCD - Complete System
+                </td>
+                <td>
+                    FLIR SAFIRE CCD
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Model SAFIRE LRF - Complete System
+                </td>
+                <td>
+                    FLIR SAFIRE LRF
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Model SAFIRE LCRS - Complete System
+                </td>
+                <td>
+                    FLIR SAFIRE LCRS
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Model SAFIRE LI - Complete System
+                </td>
+                <td>
+                    FLIR SAFIRE LI
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Model STAR SAFIRE - Complete System
+                </td>
+                <td>
+                    FLIR STAR SAFIRE
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Model Ultra 6000 - Complete System
+                </td>
+                <td>
+                    FLIR Ultra 6000 / U6000
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Model Ultra 7000 - Complete System
+                </td>
+                <td>
+                    FLIR Ultra 7000 / U7000d
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Inframetrics / FLIR
+                </td>
+                <td>
+                    Model 445G MK I - Complete System
+                </td>
+                <td>
+                    Inframetrics / FLIR Mark I
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Inframetrics / FLIR
+                </td>
+                <td>
+                    Model 445G MK II - Complete System
+                </td>
+                <td>
+                    Inframetrics / FLIR Mark II
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Inframetrics / FLIR
+                </td>
+                <td>
+                    Model 445G MK III Quantum - Complete System
+                </td>
+                <td>
+                    Inframetrics / FLIR Mark III
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Mount
+                </td>
+                <td>
+                </td>
+                <td>
+                    17398
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Pump, Vacuum
+                </td>
+                <td>
+                    2000A
+                </td>
+                <td>
+                    75001200
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Cable, VAC Test Adapter
+                </td>
+                <td>
+                    64869
+                </td>
+                <td>
+                    24601413/D
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Desiccant Servicing Wrench
+                </td>
+                <td>
+                    NSCM: 3AC24
+                </td>
+                <td>
+                    62001970/A-0
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Star Isolation mount
+                </td>
+                <td>
+                </td>
+                <td>
+                    64869-75002114
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Isolation mount
+                </td>
+                <td>
+                </td>
+                <td>
+                    64869-75002119
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Service Kit
+                </td>
+                <td>
+                </td>
+                <td>
+                    64869-75002912
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Service Kit
+                </td>
+                <td>
+                </td>
+                <td>
+                    64869-75002919
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Gauge, Vacuum
+                </td>
+                <td>
+                    9VDC
+                </td>
+                <td>
+                    64869-75003040
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    2000 Series SCU / HCU - Handheld Control Unit
+                </td>
+                <td>
+                    2000 Series (FV / GV)
+                </td>
+                <td>
+                    64869-R92001543
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, inc.
+                </td>
+                <td>
+                    2000 Series SCU / HCU - Handheld Control Unit
+                </td>
+                <td>
+                    2000 Series (non IPAT)
+                </td>
+                <td>
+                    64869-92000060
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, inc.
+                </td>
+                <td>
+                    2000 Series SCU / HCU - Handheld Control Unit
+                </td>
+                <td>
+                    2000 Series (non IPAT)
+                </td>
+                <td>
+                    64869-92000060-036
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    2000 Series SCU / HCU - Handheld Control Unit
+                </td>
+                <td>
+                    2000 Series
+                </td>
+                <td>
+                    64869-92001537-2
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    2000 Series SCU / HCU - Handheld Control Unit
+                </td>
+                <td>
+                    2000 Series
+                </td>
+                <td>
+                    64869-92001539
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    2000 Series SCU / HCU - Handheld Control Unit
+                </td>
+                <td>
+                    2000 Series
+                </td>
+                <td>
+                    64869-92001543
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    2000 Series SCU / HCU - Handheld Control Unit
+                </td>
+                <td>
+                    2000 Series (w/ DCR)
+                </td>
+                <td>
+                    64869-92001561
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    Ultra 6000 Series SCU - System Control Unit
+                </td>
+                <td>
+                    U6000
+                </td>
+                <td>
+                    425/2000/200
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FLIR Systems, Inc.
+                </td>
+                <td>
+                    2000 Series IDP - Interface / Distribution / Processing Unit
+                </td>
+                <td>
+                    2000V
+                </td>
+                <td>
+                    64869-E027-8022
+                </td>
+            </tr>
+            <tr>
+                <td>
+                </td>
+                <td>
+                </td>
+                <td>
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                </td>
+                <td>
+                </td>
+                <td>
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                </td>
+                <td>
+                </td>
+                <td>
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                </td>
+                <td>
+                </td>
+                <td>
+                </td>
+                <td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                </td>
+                <td>
+                </td>
+                <td>
+                </td>
+                <td>
+                </td>
+            </tr>
+
+
             </tbody>
         </Table>
 
