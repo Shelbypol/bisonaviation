a2335848ef5af1b8c64d19b2f7012f8106c0e879
Shelby Polasek <shelbypolasek@gmail.com>
2021-03-15 14:38:03 -0500

    avionics hero carousel added and working

--- frontend/src/components/2/avionics-Screen/AvionicsHero.js
+++ frontend/src/components/2/avionics-Screen/AvionicsHero.js
@@ -6,12 +6,14 @@ const AvionicsHero = () => {
 
     return (
         <HeroCarousel
+            IDslide1={'AvionicsHero_carouselImg1'}
             slide1firstTitleLetter={'M'}
             slide1firstTitleWord={'aintenance'}
             slide1firstSentence={'Looks complicated...'}
             slide1secondSentenceTrue={true}
             slide1secondSentence={'...that\'s because it is!'}
 
+            IDslide2={'AvionicsHero_carouselImg2'}
             carouselImg2True={true}
             slide2firstTitleLetter={'F'}
             slide2firstTitleWord={'rom gliders...'}
@@ -19,6 +21,7 @@ const AvionicsHero = () => {
             slide2secondSentenceTrue={true}
             slide2secondSentence={'KNOWS YOUR AIRCRAFT INSIDE AND OUT!'}
 
+            IDslide3={'AvionicsHero_carouselImg3'}
             carouselImg3True={true}
             slide3firstTitleLetter={'I'}
             slide3firstTitleWord={'t isn\'t over until...'}
@@ -26,6 +29,7 @@ const AvionicsHero = () => {
             slide3secondSentenceTrue={true}
             slide3secondSentence={'cure your logbook log jams!'}
 
+            IDslide4={'AvionicsHero_carouselImg4'}
             carouselImg4True={true}
             slide4firstTitleLetter={'F'}
             slide4firstTitleWord={'rom minor repairs...'}
@@ -33,12 +37,13 @@ const AvionicsHero = () => {
             slide4secondSentenceTrue={true}
             slide4secondSentence={'BISON IS READY FOR YOUR MAINTENANCE PROJECT!'}
 
+            IDslide5={'AvionicsHero_carouselImg5'}
             carouselImg5True={false}
-            // // slide5firstTitleLetter={}
-            // // slide5firstTitleWord={}
-            // // slide5firstSentence={}
-            // // slide5secondSentenceTrue={}
-            // // slide5secondSentence={}
+            slide5firstTitleLetter={'F'}
+            slide5firstTitleWord={'rom minor repairs...'}
+            slide5firstSentence={'...TO COMPLETE RADOME TO TAIL CONE REFURBISHMENT.'}
+            slide5secondSentenceTrue={true}
+            slide5secondSentence={'BISON IS READY FOR YOUR MAINTENANCE PROJECT!'}
         />
     )
 };
