efb6fb6974b686be519cdaacf2bea34273efd67a
Shelby Polasek <shelbypolasek@gmail.com>
2021-03-15 14:29:59 -0500

    updated heroCarousel component to include unique id for images since scss files made classnames global

--- frontend/src/components/2/HomeHero.js
+++ frontend/src/components/2/HomeHero.js
@@ -3,16 +3,18 @@ import '../../style/2/HomeHero.css';
 import HeroCarousel from "../HeroCarousel";
 
 
-
 const HomeHero = () => {
     return (
         <HeroCarousel
+
+            IDslide1={'HomeHero_carouselImg1'}
             slide1firstTitleLetter={'M'}
             slide1firstTitleWord={'aintenance'}
             slide1firstSentence={'Looks complicated...'}
             slide1secondSentenceTrue={true}
             slide1secondSentence={'...that\'s because it is!'}
 
+            IDslide2={'HomeHero_carouselImg2'}
             carouselImg2True={true}
             slide2firstTitleLetter={'F'}
             slide2firstTitleWord={'rom gliders...'}
@@ -20,6 +22,7 @@ const HomeHero = () => {
             slide2secondSentenceTrue={true}
             slide2secondSentence={'KNOWS YOUR AIRCRAFT INSIDE AND OUT!'}
 
+            IDslide3={'HomeHero_carouselImg3'}
             carouselImg3True={true}
             slide3firstTitleLetter={'I'}
             slide3firstTitleWord={'t isn\'t over until...'}
@@ -27,6 +30,7 @@ const HomeHero = () => {
             slide3secondSentenceTrue={true}
             slide3secondSentence={'cure your logbook log jams!'}
 
+            IDslide4={'HomeHero_carouselImg4'}
             carouselImg4True={true}
             slide4firstTitleLetter={'F'}
             slide4firstTitleWord={'rom minor repairs...'}
@@ -34,13 +38,15 @@ const HomeHero = () => {
             slide4secondSentenceTrue={true}
             slide4secondSentence={'BISON IS READY FOR YOUR MAINTENANCE PROJECT!'}
 
-            carouselImg5True={false}
-            // // slide5firstTitleLetter={}
-            // // slide5firstTitleWord={}
-            // // slide5firstSentence={}
-            // // slide5secondSentenceTrue={}
-            // // slide5secondSentence={}
+            IDslide5={'HomeHero_carouselImg5'}
+            carouselImg5True={true}
+            slide5firstTitleLetter={'F'}
+            slide5firstTitleWord={'rom minor repairs...'}
+            slide5firstSentence={'...TO COMPLETE RADOME TO TAIL CONE REFURBISHMENT.'}
+            slide5secondSentenceTrue={true}
+            slide5secondSentence={'BISON IS READY FOR YOUR MAINTENANCE PROJECT!'}
         />
+
     );
 };
 
