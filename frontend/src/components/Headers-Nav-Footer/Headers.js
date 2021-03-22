f2460d0a94a840320445ff3c1c79fed1d252e420
Shelby Polasek <shelbypolasek@gmail.com>
2021-03-22 10:47:22 -0500

    desktop header bg dots added with margin and height adjustment

--- frontend/src/components/Headers-Nav-Footer/Headers.js
+++ frontend/src/components/Headers-Nav-Footer/Headers.js
@@ -3,6 +3,7 @@ import MobileHeader from "./MobileHeader";
 import DesktopHeader from "./DesktopHeader";
 import '../../style/headers.css'
 import '../../style/global-1.css'
+import Fade from "react-reveal/Fade";
 
 const Headers = () => {
     const [width, setWidth] = useState(window.innerWidth);
