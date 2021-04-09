b5ac2184730c6c520924282cedb84c14dde6fad9
Shelby Polasek <shelbypolasek@gmail.com>
2021-04-09 16:08:53 -0500

    background-body requires rgba(000.5) overlay for official use / summary nav movibe repsonsive

--- frontend/src/components/home-screen/Services.js
+++ frontend/src/components/home-screen/Services.js
@@ -9,117 +9,104 @@ const Services = () => {
     return (
 
         <>
-            {/*<Container className='p-0' fluid>*/}
+            {/*<Container className='p-0 min-vh-100' fluid>*/}
 
 
-            <Row className=' Services_row px-5 pt-5 mb-3 global_blue d-flex justify-content-between'>
+            <Row className=' Services_row p-5 mb-3 global_blue d-flex justify-content-center'>
 
-                {/*<Col lg={10} xs={12} className='d-flex justify-content-center'>*/}
-                {/*    <Col xs={12} md={6}>*/}
-                {/*<Row className='mb-5'>*/}
-                <Col xs={12} md={4}>
+                {/*======================   PRODUCTS / MAINTENANCE COL     ======================*/}
+                <Col xs={12} md={5} className=''>
                     <Col xs={12} className='Services_nav_hoverTitle mb-3'>
                         <Zoom>
-                            <Col className='Services_nav_overlay'>
-                                <p className='Services_nav_text' style={{color: 'white'}}>Products</p>
+                            <Col>
+                                <p className='Services_nav_text d-flex justify-content-end'>Products</p>
                             </Col>
                         </Zoom>
                     </Col>
-                    <Row xs={12}>
+                    <Row xs={12} className='Services_text-row'>
                         <Col xs={2} className='d-flex justify-content-center mt-3'>
                             <i className='far fa-tablet-rugged global_blood-red'
                                style={{fontSize: '3em'}}> </i>
                         </Col>
                         <Col xs={8}>
                             <p>
-                                Authorized garmin dealer.
+                                Authorized Garmin dealer.
                             </p>
                         </Col>
                     </Row>
-                    {/*</Col>*/}
-                    {/*</Row>*/}
 
-                    {/*<Row className='mb-5 pt-4'>*/}
-                    {/*<Col xs={12} md={4}>*/}
-                    <Col xs={12} className='Services_nav_hoverTitle my-5'>
+
+                    <Col xs={12} className='Services_nav_hoverTitle mb-3'>
                         <Zoom>
-                            <Col className='Services_nav_overlay'>
-                                <p className='Services_nav_text' style={{color: 'white'}}>Maintenance</p>
+                            <Col>
+                                <p className='Services_nav_text d-flex justify-content-end'>Maintenance</p>
                             </Col>
                         </Zoom>
                     </Col>
-                    <Row xs={12}>
+                    <Row xs={12} className='Services_text-row'>
                         <Col xs={2} className='d-flex justify-content-center mt-3'>
-                            <i className='fas fas fa-cogs global_blood-red'
+                            <i className='far fa-camera-home global_blood-red'
                                style={{fontSize: '3em'}}> </i>
                         </Col>
                         <Col xs={8}>
                             <p>
-                                Simple skin replacements, structures fabrication, CNC machining, to laser engraving & 3D
-                                printing.
+                                Simple skin replacement, structures, fabrication, CNC machining, to laser engraving & 3D printing
                             </p>
                         </Col>
                     </Row>
+
+
                 </Col>
-                {/*</Row>*/}
-                {/*</Col>*/}
 
-                <Col xs={12} md={4}>
 
-                    <Row className='mb-5  '>
-                        <Col xs={12}>
-                            <Col xs={12} className='Services_nav_hoverTitle mb-3'>
-                                <Zoom>
-                                    <Col className='Services_nav_overlay'>
-                                        <p className='Services_nav_text' style={{color: 'white'}}>Avionics</p>
-                                    </Col>
-                                </Zoom>
+                <Col xs={12} md={5}>
+                    <Col xs={12} className='Services_nav_hoverTitle mb-3'>
+                        <Zoom>
+                            <Col>
+                                <p className='Services_nav_text d-flex justify-content-end'>Avionics</p>
                             </Col>
-                            <Row xs={12}>
-                                <Col xs={2} className='d-flex justify-content-center mt-3'>
-                                    <i className='far fa-headset global_blood-red'
-                                       style={{fontSize: '3em'}}> </i>
-                                </Col>
-                                <Col xs={8}>
-                                    <p>
-                                        From minor repairs to complete panel upgrades. All services are performed in
-                                        house...
-                                    </p>
-                                </Col>
-                            </Row>
+                        </Zoom>
+                    </Col>
+                    <Row xs={12} className='Services_text-row'>
+                        <Col xs={2} className='d-flex justify-content-center mt-3'>
+                            <i className='far fa-headset global_blood-red'
+                               style={{fontSize: '3em'}}> </i>
+                        </Col>
+                        <Col xs={8}>
+                            <p>
+                                From minor repairs to complete panel upgrades. All services are performed in
+                                house...
+                            </p>
                         </Col>
                     </Row>
 
 
-                    <Row className='mb-5'>
-                        <Col xs={12}>
-                            <Col xs={12} className='Services_nav_hoverTitle mb-3'>
-                                <Zoom>
-                                    <Col className='Services_nav_overlay'>
-                                        <p className='Services_nav_text' style={{color: 'white'}}>EO/IR Flir</p>
-                                    </Col>
-                                </Zoom>
+                    <Col xs={12} className='Services_nav_hoverTitle mb-3'>
+                        <Zoom>
+                            <Col>
+                                <p className='Services_nav_text d-flex justify-content-end'>EO/IR Flir</p>
                             </Col>
-                            <Row xs={12}>
-                                <Col xs={2} className='d-flex justify-content-center mt-3'>
-                                    <i className='far fa-camera-home global_blood-red'
-                                       style={{fontSize: '3em'}}> </i>
-                                </Col>
-                                <Col xs={8}>
-                                    <p>
-                                        Bison Aviation is the world's leader in legacy airborne EO / IR equipment
-                                        service and sales.
-                                    </p>
-                                </Col>
-                            </Row>
+                        </Zoom>
+                    </Col>
+                    <Row xs={12} className='Services_text-row'>
+                        <Col xs={2} className='d-flex justify-content-center mt-3'>
+                            <i className='far fa-camera-home global_blood-red'
+                               style={{fontSize: '3em'}}> </i>
+                        </Col>
+                        <Col xs={8}>
+                            <p>
+                                Bison Aviation is the world's leader in legacy airborne EO / IR equipment
+                                service and sales.
+                            </p>
                         </Col>
                     </Row>
 
+
                 </Col>
-                {/*</Col>*/}
 
 
-                <Col xs={12} md={4} className='d-none d-md-block'>
+
+                <Col xs={12} md={2} className='d-none d-md-block'>
                     <Row>
                         <Col xs={12} className='Services_form'>
 
--- frontend/src/screens/HomeScreen2.js
+++ frontend/src/screens/HomeScreen2.js
@@ -6,8 +6,8 @@ import Services from "../components/home-screen/Services";
 import Avionics from "../components/home-screen/Avionics";
 import Mechanics from "../components/home-screen/Mechanics";
 import Flir from "../components/home-screen/Flir";
-import { Container} from 'react-bootstrap';
-import { Jumbotron } from "reactstrap";
+import {Container} from 'react-bootstrap';
+import {Jumbotron} from "reactstrap";
 
 
 const HomeScreen2 = () => {
@@ -25,41 +25,45 @@ const HomeScreen2 = () => {
 
             <HomeHero/>
 
-            <Jumbotron className='Global_graphic bg-transparent mb-0 pb-0' fluid> </Jumbotron>
+            <Jumbotron className='Global_graphic mb-0 pb-0' style={{backgroundColor: 'rgba(0,0,0,.5)'}}
+                       fluid> </Jumbotron>
+            <Container className='m-0 p-0' style={{backgroundColor: 'rgba(0,0,0,.5)'}} fluid>
+                <RepairAward/>
 
-            <RepairAward/>
+                {/*<Container className='p-0 fluid'>*/}
+                {/*    <hr className='global_cultured-bg'/>*/}
+                {/*</Container>*/}
 
-            {/*<Container className='p-0 fluid'>*/}
-            {/*    <hr className='global_cultured-bg'/>*/}
-            {/*</Container>*/}
-
-            {/*<Container className='p-0 m-0 bg-white' fluid>*/}
-                <Services
-                    showTag={true}
-                />
-            {/*</Container>*/}
-            <hr className='global_blood-red-bg'/>
-
-            <Jumbotron className='Global_Avionics_bg-img p-0 mb-1 mt-n3 d-flex align-content-stretch bg-transparent'
-                       fluid>
-                <Container className='p-0 fluid'>
-                    <Avionics/>
+                <Container className='m-5 p-0' fluid>
+                    <Services
+                        showTag={true}
+                    />
                 </Container>
-            </Jumbotron>
+                <hr className='global_blood-red-bg'/>
 
-            <hr className='global_blood-red-bg mt-n1'/>
-            <Container className='p-0 fluid'>
-                <Mechanics/>
+                <Jumbotron className='Global_Avionics_bg-img p-0 mb-1 mt-n3 d-flex align-content-stretch bg-transparent'
+                           fluid>
+                    <Container className='p-0 fluid'>
+                        <Avionics/>
+                    </Container>
+                </Jumbotron>
+
+                <hr className='global_blood-red-bg mt-n1'/>
+                <Container className='p-0 fluid'>
+                    <Mechanics/>
+                </Container>
+
+
+                <hr className='global_blood-red-bg mt-n1'/>
+                <Jumbotron
+                    className='Global_Flir_bg-img mt-n3 mb-0 p-0 pb-5 d-flex align-content-stretch bg-transparent'
+                    fluid>
+                    <Container className='p-0 fluid'>
+                        <Flir/>
+                    </Container>
+                </Jumbotron>
+
             </Container>
-
-
-            <hr className='global_blood-red-bg mt-n1'/>
-            <Jumbotron className='Global_Flir_bg-img mt-n3 mb-0 p-0 pb-5 d-flex align-content-stretch bg-transparent'
-                       fluid>
-                <Container className='p-0 fluid'>
-                    <Flir/>
-                </Container>
-            </Jumbotron>
 
         </>
 
