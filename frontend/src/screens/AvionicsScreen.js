6b488d256e052c0c25943a784499242d75a24a70
Shelby Polasek <shelbypolasek@gmail.com>
2021-04-09 07:38:31 -0500

    sticky header & footer added to avionics / maintenance screen

--- frontend/src/screens/AvionicsScreen.js
+++ frontend/src/screens/AvionicsScreen.js
@@ -1,15 +1,47 @@
-import React, {useLayoutEffect} from 'react'
+import React, {useEffect, useLayoutEffect, useState} from 'react'
 import AvionicsHero from "../components/avionics-Screen/AvionicsHero";
+import MobileHeader from "../components/Headers-Nav-Footer/MobileHeader";
+import StickyHeader from "../components/Headers-Nav-Footer/StickyHeader";
+import {useDispatch} from "react-redux";
+import Footer from "../components/Headers-Nav-Footer/Footer";
+
 
 const AvionicsScreen = () => {
+
+    const [width, setWidth] = useState(window.innerWidth);
+
+    const breakpoint = 1000;
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+
+        window.addEventListener("resize", handleWindowResize);
+        return () => window.removeEventListener("resize", handleWindowResize);
+    }, [dispatch, width]);
 
     useLayoutEffect(() => {
         window.scrollTo(0, 0)
     });
 
+    const handleWindowResize = () => {
+        setWidth(window.innerWidth);
+    };
+
     return (
         <>
+
+            {width < breakpoint ? (
+                <MobileHeader/>
+            ) : (
+                <>
+                    <StickyHeader/>
+                </>
+            )}
+
             <AvionicsHero/>
+
+            <Footer/>
         </>
     )
 };
--- frontend/src/screens/MaintenanceScreen.js
+++ frontend/src/screens/MaintenanceScreen.js
@@ -1,18 +1,48 @@
-import React, {useLayoutEffect} from 'react'
+import React, {useEffect, useLayoutEffect, useState} from 'react'
 import MechanicsHero from "../components/mechanics-screen/MechanicsHero";
 import AvionicsIcons from "../components/avionics-Screen/AvionicsIcons";
+import {useDispatch} from "react-redux";
+import MobileHeader from "../components/Headers-Nav-Footer/MobileHeader";
+import StickyHeader from "../components/Headers-Nav-Footer/StickyHeader";
+import Footer from "../components/Headers-Nav-Footer/Footer";
 
 
 const MaintenanceScreen = () => {
+
+    const [width, setWidth] = useState(window.innerWidth);
+
+    const breakpoint = 1000;
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+
+        window.addEventListener("resize", handleWindowResize);
+        return () => window.removeEventListener("resize", handleWindowResize);
+    }, [dispatch, width]);
 
     useLayoutEffect(() => {
         window.scrollTo(0, 0)
     });
 
+    const handleWindowResize = () => {
+        setWidth(window.innerWidth);
+    };
+
     return (
         <>
+
+            {width < breakpoint ? (
+                <MobileHeader/>
+            ) : (
+                <>
+                    <StickyHeader/>
+                </>
+            )}
+
                 <MechanicsHero/>
                 <AvionicsIcons/>
+                <Footer/>
         </>
     )
 };
