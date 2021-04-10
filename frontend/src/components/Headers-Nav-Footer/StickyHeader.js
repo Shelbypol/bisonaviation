08e956227e2ce3de98a2d498761dd15b38af5c25
Shelby Polasek <shelbypolasek@gmail.com>
2021-04-10 17:26:41 -0500

    sticky header fully responsive and working on desktop / contact btn not finished styling

--- frontend/src/components/Headers-Nav-Footer/StickyHeader.js
+++ frontend/src/components/Headers-Nav-Footer/StickyHeader.js
@@ -183,68 +183,6 @@ const StickyHeader = () => {
                             {/*</Nav>*/}
 
 
-                            {/*============= NAV SHOW ON SCROLL ==============*/}
-                            {/*     <Nav xs={6} className='border*/}
-                            {/*d-flex justify-content-start StickyHeader_nav'>*/}
-
-
-                            {/*    /!*  HOME  *!/*/}
-                            {/*    <LinkContainer className='mx-1' to='/'>*/}
-                            {/*        <Nav.Link>*/}
-                            {/*            <i className='far fa-home StickyHeader_links'*/}
-                            {/*               style={{fontSize: '1.5em'}}> </i>*/}
-                            {/*        </Nav.Link>*/}
-                            {/*    </LinkContainer>*/}
-
-                            {/*    /!*  BROWSE PRODUCTS  *!/*/}
-                            {/*    <LinkContainer className='mx-1' to='/products'>*/}
-                            {/*        <Nav.Link className=''>*/}
-                            {/*        <span className='StickyHeader_links'>*/}
-                            {/*        Products*/}
-                            {/*        </span>*/}
-                            {/*        </Nav.Link>*/}
-                            {/*    </LinkContainer>*/}
-
-                            {/*    /!*  AVIONICS  *!/*/}
-                            {/*    <LinkContainer className='mx-1' to='/avionics'>*/}
-                            {/*        <Nav.Link>*/}
-                            {/*        <span className='StickyHeader_links'>*/}
-                            {/*        Avionics*/}
-                            {/*        </span>*/}
-                            {/*        </Nav.Link>*/}
-                            {/*    </LinkContainer>*/}
-
-                            {/*    /!*  MAINTENANCE  *!/*/}
-                            {/*    <LinkContainer className='mx-1' to='/maintenance'>*/}
-                            {/*        <Nav.Link>*/}
-                            {/*        <span className='StickyHeader_links'>*/}
-                            {/*        Maintenance*/}
-                            {/*        </span>*/}
-                            {/*        </Nav.Link>*/}
-                            {/*    </LinkContainer>*/}
-
-                            {/*    /!*  EO/IR FLIR  *!/*/}
-                            {/*    <LinkContainer className='mx-1' to='/flir'>*/}
-                            {/*        <Nav.Link>*/}
-                            {/*        <span className='StickyHeader_links'>*/}
-                            {/*        Flir*/}
-
-                            {/*        </span>*/}
-                            {/*        </Nav.Link>*/}
-                            {/*    </LinkContainer>*/}
-
-                            {/*    /!*  TEAM  *!/*/}
-                            {/*    <LinkContainer className='mx-1' to='/team'>*/}
-                            {/*        <Nav.Link>*/}
-                            {/*        <span className='StickyHeader_links'>*/}
-                            {/*        Team*/}
-                            {/*        </span>*/}
-                            {/*        </Nav.Link>*/}
-                            {/*    </LinkContainer>*/}
-
-                            {/*</Nav>*/}
-
-
                             {/*============= PROFILE BTNS ==============*/}
                             <Nav xs={3} className='d-flex justify-content-end pr-3'>
                                 <Nav.Link
@@ -253,18 +191,18 @@ const StickyHeader = () => {
                                 </Nav.Link>
 
                                 {!userInfo && (
-                                    <SideBar/>
+                                    <Nav.Link className='d-flex justify-content-center'>
+                                        <SideBar/>
+
+                                    </Nav.Link>
                                 )}
 
                                 <LinkContainer to='/contact'>
-                                    <Nav.Link>
-                                        <Button
-                                            className='py-0 rounded StickyHeader_contact-btn StickyHeader_iconHidden '>
-                                                    <span className='StickyHeader_hideIcon'>
-                                                     Contact
-                                                 </span>
-                                            <span className='StickyHeader_hideWords'>Contact</span>
-                                        </Button>
+                                    <Nav.Link  className='d-flex justify-content-center align-items-center StickyHeader_contact-btn'>
+                                        {/*<span*/}
+                                        {/*    className='StickyHeader_contact-btn px-2 py-0'>*/}
+                                            Contact
+                                        {/*</span>*/}
                                     </Nav.Link>
                                 </LinkContainer>
 
