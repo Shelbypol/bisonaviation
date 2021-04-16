664a0162968848e71f97628d897ff256ab1044ba
Shelby Polasek <shelbypolasek@gmail.com>
2021-04-16 09:17:56 -0500

    contact btn hover updated

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -17,7 +17,6 @@ const getProducts = asyncHandler(async (req, res) => {
         }
     } : {};
 
-
     const count = await Product .countDocuments({ ...keyword });
     const products = await Product.find({ ...keyword }).limit(pageSize).skip(pageSize * (page - 1));
     res.json({ products, page, pages: Math.ceil(count / pageSize)})
