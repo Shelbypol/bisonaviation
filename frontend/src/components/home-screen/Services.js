f80d4d24c63de1ec1b2da84f750617445a2a9c4f
Shelby Polasek <shelbypolasek@gmail.com>
2021-04-21 17:57:17 -0500

    services nav rendering desktop view correctly

--- frontend/src/components/home-screen/Services.js
+++ frontend/src/components/home-screen/Services.js
@@ -10,29 +10,13 @@ const Services = () => {
     return (
 
         <>
-            <Row className='text-white text-center my-5'>
+            <Row className='text-white text-center my-5 mx-2'>
 
-                {/*<Col md={6} lg={3} className='d-flex justify-content-center global_cursor'>*/}
-                {/*    <Card className='bg-transparent border-0 w-100'>*/}
-                {/*        <Card.Body>*/}
-                {/*            <Card.Title className='Services_nav_text m-0'> Products</Card.Title>*/}
-                {/*            <div className='Services_text w-100 h-100 d-flex align-items-center justify-content-center'>*/}
-                {/*                <Card.Text*/}
-                {/*                    className='my-0 p-0'>*/}
-                {/*                    Authorized Garmin dealer.*/}
-                {/*                </Card.Text>*/}
-                {/*                <br/>*/}
-                {/*          */}
-                {/*            </div>*/}
-                {/*        </Card.Body>*/}
-                {/*    </Card>*/}
-                {/*</Col>*/}
-
-                <Col md={6} lg={4} className='d-flex justify-content-center'>
+                <Col md={4} className='d-flex justify-content-center'>
                     <Card className='bg-transparent border-0 w-100'>
-                        <Card.Body>
                             <Card.Title className='Services_nav_text mx-auto'> Maintenance</Card.Title>
-                            <Card.Text className='Services_text w-100 h-100 d-flex justify-content-center align-items-center'>
+                        <Card.Body className='Services_text w-100 h-100 d-flex justify-content-center align-items-center'>
+                            <Card.Text>
                                 Simple skin replacement, structures, fabrication, CNC machining, to laser engraving & 3D
                                 printing
                             </Card.Text>
@@ -40,26 +24,29 @@ const Services = () => {
                     </Card>
                 </Col>
 
-                <Col md={6} lg={4} className='d-flex justify-content-center'>
+                <Col md={4} className='d-flex justify-content-center'>
                     <Card className='bg-transparent border-0 w-100'>
-                        <Card.Body>
-                            <Card.Title className='Services_nav_text mx-auto'> Avionics</Card.Title>
-                            <Card.Text className='Services_text w-100 h-100 d-flex justify-content-center align-items-center'>
+                        <Card.Title className='Services_nav_text mx-auto '> Avionics</Card.Title>
+                        <Card.Body
+                            className='Services_text w-100 h-100 d-block d-flex justify-content-center align-items-center'>
+                            <Card.Text>
                                 From minor repairs to complete panel upgrades. All services are performed in
-                                house.
+                                house. <span><Card.Link href="/products" className='mt-n2 mx-2 global_blue'>browse
+                                products
+                            </Card.Link></span>
                             </Card.Text>
+
+
                         </Card.Body>
-                        <Card.Link href="/products" className='global_blue'>browse
-                            products
-                        </Card.Link>
                     </Card>
                 </Col>
 
-                <Col md={6} lg={4} className='d-flex justify-content-center'>
+                <Col md={4} className='d-flex justify-content-center'>
                     <Card className='bg-transparent border-0 w-100'>
-                        <Card.Body>
-                            <Card.Title className='Services_nav_text mx-auto'>EO/IR Flir</Card.Title>
-                            <Card.Text className='Services_text w-100 h-100 d-flex justify-content-center align-items-center'>
+                        <Card.Title className='Services_nav_text mx-auto'>EO/IR Flir</Card.Title>
+                        <Card.Body
+                            className='Services_text w-100 h-100 d-flex justify-content-center align-items-center'>
+                            <Card.Text>
                                 World leader in legacy airborne EO / IR equipment
                                 service and sales.
                             </Card.Text>
