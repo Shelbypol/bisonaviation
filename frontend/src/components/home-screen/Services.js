426153fc40200942ad3bf8b1199de75b100ac974
Shelby Polasek <shelbypolasek@gmail.com>
2021-04-22 13:28:21 -0500

    services icon added & desktop styled

--- frontend/src/components/home-screen/Services.js
+++ frontend/src/components/home-screen/Services.js
@@ -10,19 +10,23 @@ const Services = () => {
     return (
 
         <>
-            <Row className='text-white text-center my-5 mx-2'>
-                {/*<Col xs={12} className='d-flex justify-content-center font-weight-lighter'>*/}
-                {/*    <h3 className='Services_nav_text'>Services</h3>*/}
-                {/*</Col>*/}
+            <Row className='Services_row text-white text-center my-0 d-flex justify-content-around'>
 
-                <Col md={4} className='d-flex justify-content-center global_cursor'>
-                    <Card className='bg-transparent border-0 w-100'>
-                        <Card.Title className=' mx-auto my-0'><h5 className='Services_nav_text'>Maintenance</h5>
-                        </Card.Title>
-                        <Card.Body
-                            className='Services_text w-100 h-100 d-flex justify-content-center align-items-center'>
-                            <Card.Text className='text-uppercase'>
-                                <h6 className=' Services_description'>
+                <Col xs={12} className='mb-n3 pb-0' style={{backgroundColor: 'rgba(0,0,0,.4)', height: '10vh'}}>
+
+                </Col>
+
+                <Col md={3} className='mt-n5 d-flex justify-content-center global_cursor'>
+                    <Card className='p-2 border-0 w-100 Services_bg'>
+                        <Card.Body>
+                            <Card.Title className='mx-auto my-0 w-25 border rounded-circle px-2 py-3 global_blood-red-bg'>
+                                <i className='Services_icon fas fa-cart-arrow-down'> </i>
+                            </Card.Title>
+                            <Card.Subtitle className='mx-auto my-3'>
+                                <h5 className='Services_nav_text'>Maintenance</h5>
+                            </Card.Subtitle>
+                            <Card.Text>
+                                <h6 className='Services_description'>
                                     Simple skin replacement, structures, fabrication, CNC machining, to laser engraving
                                     & 3D
                                     printing
@@ -32,32 +36,40 @@ const Services = () => {
                     </Card>
                 </Col>
 
-                <Col md={4} className='d-flex justify-content-center global_cursor'>
-                    <Card className='bg-transparent border-0 w-100'>
-                        <Card.Title className=' mx-auto my-0'><h5 className='Services_nav_text'>Avionics</h5>
-                        </Card.Title>
-                        <Card.Body
-                            className='Services_text w-100 h-100 d-block d-flex justify-content-center align-items-center'>
+                <Col md={3} className='mt-n5 d-flex justify-content-center global_cursor'>
+                    <Card className='p-2 border-0 w-100 Services_bg'>
+                        <Card.Body>
+                            <Card.Title className='mx-auto my-0 w-25 border rounded-circle px-2 py-3 global_blood-red-bg'>
+                                    <i className='Services_icon fas fa-cart-arrow-down'> </i>
+                            </Card.Title>
+                            <Card.Subtitle className='mx-auto my-3'>
+                                <h5 className='Services_nav_text'>Avionics</h5>
+                            </Card.Subtitle>
                             <Card.Text>
-                                <h6 className=' Services_description'>
+                                <h6 className='Services_description'>
                                     From minor repairs to complete panel upgrades. All services are performed in
-                                    house. <span><Card.Link href="/products" className='mt-n2 mx-2 global_blue'>browse
-                                products
-                            </Card.Link></span>
+                                    house.
+                                    <span>
+                                    </span>
                                 </h6>
                             </Card.Text>
-
-
+                            <Card.Link href="/products" className='w-50 mt-n2 px-2 mx-auto px-auto Services_link'>browse
+                                products
+                            </Card.Link>
                         </Card.Body>
                     </Card>
                 </Col>
 
-                <Col md={4} className='d-flex justify-content-center global_cursor'>
-                    <Card className='bg-transparent border-0 w-100'>
-                        <Card.Title className=' mx-auto my-0'><h5 className='Services_nav_text'>EO/IR Flir</h5>
-                        </Card.Title>
+                <Col md={3} className='mt-n5 d-flex justify-content-center global_cursor'>
+                    <Card className='p-2 border-0 w-100 Services_bg'>
                         <Card.Body
-                            className='Services_text w-100 h-100 d-flex justify-content-center align-items-center'>
+                            className=''>
+                            <Card.Title className='mx-auto my-0 w-25 border rounded-circle px-2 py-3 global_blood-red-bg'>
+                                <i className='Services_icon fas fa-cart-arrow-down'> </i>
+                            </Card.Title>
+                            <Card.Subtitle className='mx-auto my-3'>
+                                <h5 className='Services_nav_text'>EO/IR Flir</h5>
+                            </Card.Subtitle>
                             <Card.Text>
                                 <h6 className=' Services_description'>
                                     World leader in legacy airborne EO / IR equipment
