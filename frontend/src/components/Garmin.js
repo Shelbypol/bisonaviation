a72176692a2413f408c13577c946a43280810f36
Shelby Polasek <shelbypolasek@gmail.com>
2021-05-12 12:17:35 -0500

    cleaned up garmin component

--- frontend/src/components/Garmin.js
+++ frontend/src/components/Garmin.js
@@ -12,51 +12,25 @@ const Garmin = () => {
 
 
     return (
-
-        // <Jumbotron className=' m-0 pt-4 pb-0 px-0 bg-transparent' fluid>
-
-            <Row className='d-flex justify-content-around m-0 p-0 Garmin_card-row border'>
-
+            <Row className='d-flex justify-content-around m-0 p-0 Garmin_card-row'>
                 <Col xs={5} className='bg-transparent d-flex justify-content-center'>
                     <Fade duration={1500} right>
-                        {/*<Row>*/}
-                        {/*    <Col className='m-0 p-0'>*/}
-                        {/*<Card className='border-0 bg-transparent'>*/}
                         <Card style={{width: '18rem'}} className='border-0 bg-transparent'>
                             <Card.Body>
                                 <ProductCarousel/>
-                                {/*<Card.Title className='global_blood-red'>5% OFF Tagged Products</Card.Title>*/}
-                                {/*<Card.Text>*/}
-                                {/*    Any products marked with a red flag receive a 5% discount!*/}
-                                {/*</Card.Text>*/}
-                                {/*<Card.Subtitle className="mb-2 text-muted"> PROMO ENDS March 1st, 2021</Card.Subtitle>*/}
-                                {/*<Card.Link href="#">Browse Products</Card.Link>*/}
                             </Card.Body>
                         </Card>
-                        {/*</Col>*/}
-                        {/*<Col className=''>*/}
-                        {/*    <Card style={{width: '18.2rem'}} className='border-0 bg-transparent'>*/}
-                        {/*        /!*<Card.Subtitle className='Garmin_flag red global_accentFont mt-n1 mb-0 mx-0'>sale</Card.Subtitle>*!/*/}
-                        {/*        /!*<Card.Img variant="top" src=/>*!/*/}
-                        {/*    </Card>*/}
-                        {/*</Col>*/}
-
-                        {/*</Row>*/}
                     </Fade>
                 </Col>
 
-
-                <Col xs={7} className=''>
+                <Col xs={7}>
                     <AvionicsProducts/>
                 </Col>
-
-
-
 
             </Row>
 
 
-        // </Jumbotron>
+
 
     )
 };
