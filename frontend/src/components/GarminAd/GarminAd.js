fe38ea19b4e52ebda0165bb82f89d1d817a7d530
Shelby Polasek <shelbypolasek@gmail.com>
2021-05-21 16:01:17 -0500

    garmin file structure and naming convention updated & garmin ad fully desktop and mobile responsive

--- frontend/src/components/GarminAd/GarminAd.js
+++ frontend/src/components/GarminAd/GarminAd.js
@@ -1,31 +1,30 @@
 import React from 'react';
-import {Col, Button, Row} from "react-bootstrap";
+import {Col, Button, Row, Image} from "react-bootstrap";
 import Fade from 'react-reveal/Fade';
 import {LinkContainer} from "react-router-bootstrap";
 
+import garminad from './garmin-ad.png'
 
 
 const GarminAd = () => {
 
         return (
             <>
-                <Row className=' d-flex align-content-center'>
-                    <Fade duration={1500} left>
-                        <Col md={12} xs={8} className='GarminAd_img '>
-                            <p className='GarminAd_flag red global_accentFont mt-n1 mb-0 mx-0'>NEW!</p>
-                            <Fade duration={1500} right>
-                                <LinkContainer to={`/products`}>
-                                    <div
-                                        className=' global_cultured d-flex justify-content-center align-items-center'>
-                                        <Button className='GarminAd_browse-products-btn global_blood-red-bg py-2 px-5'>Browse
-                                            Products</Button>
-                                    </div>
-                                </LinkContainer>
-                            </Fade>
-                        </Col>
-                    </Fade>
+            <Row className=' d-flex align-content-center pb-2'>
+                    {/*<Fade duration={1500} top>*/}
+                <Col xs={12}>
+                        <p className='GarminAd_flag red global_accentFont mt-n3 mb-0 mx-0'>NEW!</p>
+                        <Image src={garminad} fluid/>
+                </Col>
+                <Col md={9} xs={12} className='d-flex justify-content-center'>
+                        <LinkContainer to={`/products`}>
+                            <Button className='GarminAd_browse-products-btn global_blood-red-bg py-3  '>Browse
+                                Products</Button>
+                        </LinkContainer>
+                </Col>
+            {/*</Fade>*/}
 
-                </Row>
+            </Row>
             </>
         );
     }
