00f2f36ff6875d16279c66cc004ad06cff6b59da
Shelby Polasek <shelbypolasek@gmail.com>
2021-05-21 16:03:25 -0500

    margin react reveal added and working

--- frontend/src/components/GarminAd/GarminAd.js
+++ frontend/src/components/GarminAd/GarminAd.js
@@ -10,21 +10,23 @@ const GarminAd = () => {
 
         return (
             <>
-            <Row className=' d-flex align-content-center pb-2'>
-                    {/*<Fade duration={1500} top>*/}
-                <Col xs={12}>
-                        <p className='GarminAd_flag red global_accentFont mt-n3 mb-0 mx-0'>NEW!</p>
-                        <Image src={garminad} fluid/>
-                </Col>
-                <Col md={9} xs={12} className='d-flex justify-content-center'>
-                        <LinkContainer to={`/products`}>
-                            <Button className='GarminAd_browse-products-btn global_blood-red-bg py-3  '>Browse
-                                Products</Button>
-                        </LinkContainer>
-                </Col>
-            {/*</Fade>*/}
+                <Row className=' d-flex align-content-center pb-2'>
+                    <Col xs={12}>
+                        <Fade duration={1500} top>
+                            <p className='GarminAd_flag red global_accentFont mt-n3 mb-0 mx-0'>NEW!</p>
+                            <Image src={garminad} fluid/>
+                        </Fade>
+                    </Col>
+                    <Col md={9} xs={12} className='d-flex justify-content-center'>
+                        <Fade duration={1500} top>
+                            <LinkContainer to={`/products`}>
+                                <Button className='GarminAd_browse-products-btn global_blood-red-bg py-3  '>Browse
+                                    Products</Button>
+                            </LinkContainer>
+                        </Fade>
+                    </Col>
 
-            </Row>
+                </Row>
             </>
         );
     }
