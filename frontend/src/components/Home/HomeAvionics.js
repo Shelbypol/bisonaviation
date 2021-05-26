24c8bb74f365608859dc9001bfe96ae51bd301de
Shelby Polasek <shelbypolasek@gmail.com>
2021-05-26 13:11:32 -0500

    HomeAvionics fully desktop and mobile responsive w/ css uniform naming applied

--- frontend/src/components/Home/HomeAvionics.js
+++ frontend/src/components/Home/HomeAvionics.js
@@ -9,21 +9,26 @@ const HomeAvionics = () => {
 
     return (
         <>
-            <Row className='px-1 d-flex justify-content-center align-content-center'>
-                <Col xs={12} className='HomeAvionics_image p-0 mt-5 mb-0 mx-0 mb-0'>
+            <Row className='d-flex justify-content-center align-content-center'>
+                <Col xs={12} className='HomeAvionics_image p-0 mt-5 mb-0 mx-0 global_goldenrod-border-right'>
 
-                    <Row className='justify-content-start align-content-end m-0 p-0 HomeAvionics_title-row'>
-                        <Fade top >
-                            <Col xs={12} className='global_black-bg m-0 pb-1 pr-5'>
-                                <h1 className='HomeAvionics_title global_white'>Avionics</h1>
-                            </Col>
-                        </Fade>
+                    <Row className='d-flex align-content-end mx-0 mt-0 p-0 mb-n1 HomeAvionics_title-row '>
+                        <Col md={6} xs={12}
+                             className='p-0 mx-0 mb-md-n2 mb-sm-3 global_goldenrod-border-right global_black-bg'>
+                            <h2 className='HomeAvionics_title global_white text-center'>
+                                <Fade top>
+                                    Avionics
+                                </Fade>
+                            </h2>
+                        </Col>
                     </Row>
 
-                    <Row className=' pb-0 px-0 pt-3 mx-0 global_black-bg d-flex justify-content-between'>
+                    <Row
+                        className='pb-0 px-2 pt-md-3 pt-sm-0 mx-n1 mb-n1 my-0 global_black-bg d-flex justify-content-between'>
 
-                        <Col md={4} className=' mx-0 p-0 my-md-5 my-sm-0 global_goldenrod global_black-transparent global_goldenrod-border-left global_goldenrod-border-right'>
-                            <LinkContainer to={`/mechanics`}>
+                        <Col lg={4} md={6} xs={12}
+                             className='mx-0 py-0 px-2 my-md-5 my-sm-0 global_goldenrod global_black-transparent global_goldenrod-border-left global_goldenrod-border-right'>
+                            <LinkContainer to={`/avionics`}>
                                 <Col xs={12}
                                      className='HomeAvionics_titles global_cursor global_goldenrodtxt-btn Mechanics_col d-flex align-items-end justify-content-center p-0 m-0'>
                                     <Zoom>
@@ -32,7 +37,7 @@ const HomeAvionics = () => {
                                 </Col>
                             </LinkContainer>
 
-                            <LinkContainer to={`/mechanics`}>
+                            <LinkContainer to={`/avionics`}>
                                 <Col xs={12}
                                      className='HomeAvionics_titles global_cursor global_goldenrodtxt-btn Mechanics_col d-flex align-items-center justify-content-center p-0 m-0'>
                                     <Zoom>
@@ -41,7 +46,7 @@ const HomeAvionics = () => {
                                 </Col>
                             </LinkContainer>
 
-                            <LinkContainer to={`/mechanics`}>
+                            <LinkContainer to={`/avionics`}>
                                 <Col xs={12}
                                      className='HomeAvionics_titles global_cursor global_goldenrodtxt-btn Mechanics_col d-flex align-items-start justify-content-center p-0 m-0'>
                                     <Zoom>
@@ -51,17 +56,20 @@ const HomeAvionics = () => {
                             </LinkContainer>
                         </Col>
 
+                        {/*MOBILE IMAGE*/}
+                        <Col xs={11}
+                             className='HomeAvionics_panel-img HomeAvionics_img mx-auto my-0 py-0 d-sm-block d-md-none'> </Col>
 
-                        <Col md={7} xs={12} className='mx-0 p-0 my-auto'>
-                            <Zoom>
-                                <Col xs={12} className='mb-2 pr-4'>
+
+                        <Col lg={7} md={6} xs={12} className='mx-0 p-0 my-auto'>
+                            <Col xs={12} className='mb-2 pr-4 d-none d-md-block'>
+                                <Zoom>
                                     <h1 className='global_white'>You depend on your aircraft to carry you and your
                                         passengers safely.</h1>
+                                </Zoom>
+                            </Col>
 
-                                </Col>
-                            </Zoom>
-
-                            <Col xs={12} className='global_cursor mt-4 ml-2 '>
+                            <Col xs={12} className='global_cursor mt-4 ml-2 text-center text-md-left'>
                                 <Zoom>
                                     <p className='pr-3 global_white'>
                                         Whether you need to add ADS-B OUT capabilities in order to meet the December
@@ -70,52 +78,34 @@ const HomeAvionics = () => {
                                         time
                                         for a completely new panel, Bison Aviation's Avionics Team is ready to bring
                                         your avionics systems into the 21st century!
-
                                     </p>
                                 </Zoom>
                             </Col>
 
-                            <Col xs={12} className='py-1 py-sm-1 global_cursor ml-lg-2'>
-                                <LinkContainer to={`/maintenance`}>
-                                    <Zoom>
-                                        <Button className='global_dark-bg-btn d-sm-flex justify-content-sm-center justify-content-md-start'>
+                            <Col md={4} xs={12} className='global_cursor ml-lg-2 mb-5 pb-5 pt-1'>
+                                <LinkContainer to={`/avionics`}>
+                                    <Button
+                                        className='global_dark-bg-btn d-flex justify-content-sm-center text-center'>
+                                        <Zoom>
                                             Learn More
-                                        </Button>
-                                    </Zoom>
+                                        </Zoom>
+                                    </Button>
                                 </LinkContainer>
                             </Col>
 
                         </Col>
                     </Row>
-
                 </Col>
 
-                <Col xs={12} className='m-0 pt-sm-0 px-0 global_black'>
-                    <Row className='m-0 p-0 global_black'>
-                        <Col xs={12} className=''>
-                            <Row className='d-flex justify-content-between'>
+                <Col xs={12}
+                     className='HomeAvionics_panel-img HomeAvionics_img d-none d-md-block global_goldenrod-border-right'> </Col>
 
 
-                                <Col className='m-0 p-0 HomeAvionics_tayseer-img HomeAvionics_img'>
-                                </Col>
-                                <Col className='mx-3 p-0 HomeAvionics_img HomeAvionics_cnc-img'>
-                                </Col>
-                                <Col className='m-0 p-0 HomeAvionics_roel-img HomeAvionics_img'>
-                                </Col>
-                                <Col md={12} className='HomeAvionics_panel-img HomeAvionics_img mt-3'>
-                                </Col>
-
-                            </Row>
-
-
-                        </Col>
-
-                    </Row>
-                </Col>
             </Row>
-        </>
+            <
+            />
 
-    )
-};
+            )
+            };
 
-export default HomeAvionics
+            export default HomeAvionics
