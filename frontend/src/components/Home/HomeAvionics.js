8397ef7810575967c30f27c3952f105d68392ec5
Shelby Polasek <shelbypolasek@gmail.com>
2021-05-26 13:51:31 -0500

    learn more btn fully responsive

--- frontend/src/components/Home/HomeAvionics.js
+++ frontend/src/components/Home/HomeAvionics.js
@@ -12,7 +12,7 @@ const HomeAvionics = () => {
             <Row className='d-flex justify-content-center align-content-center'>
                 <Col xs={12} className='HomeAvionics_image p-0 mt-5 mb-0 mx-0 global_goldenrod-border-right'>
 
-                    <Row className='d-flex align-content-end mx-0 mt-0 p-0 mb-n1 HomeAvionics_title-row '>
+                    <Row className='d-flex align-content-end mx-0 mt-0 p-0 mb-n1 ml-n1 HomeAvionics_title-row '>
                         <Col md={6} xs={12}
                              className='p-0 mx-0 mb-md-n2 mb-sm-3 global_goldenrod-border-right global_black-bg'>
                             <h2 className='HomeAvionics_title global_white text-center'>
@@ -26,11 +26,11 @@ const HomeAvionics = () => {
                     <Row
                         className='pb-0 px-2 pt-md-3 pt-sm-0 mx-n1 mb-n1 my-0 global_black-bg d-flex justify-content-between'>
 
-                        <Col lg={4} md={6} xs={12}
+                        <Col lg={6} md={5} xs={12}
                              className='mx-0 py-0 px-2 my-md-5 my-sm-0 global_goldenrod global_black-transparent global_goldenrod-border-left global_goldenrod-border-right'>
                             <LinkContainer to={`/avionics`}>
                                 <Col xs={12}
-                                     className='HomeAvionics_titles global_cursor global_goldenrodtxt-btn Mechanics_col d-flex align-items-end justify-content-center p-0 m-0'>
+                                     className='HomeAvionics_titles global_cursor global_goldenrodtxt-btn Mechanics_col d-flex align-items-center justify-content-center p-0 m-0'>
                                     <Zoom>
                                         <p className='HomeAvionics_text-titles'>Annual Inspections</p>
                                     </Zoom>
@@ -48,12 +48,22 @@ const HomeAvionics = () => {
 
                             <LinkContainer to={`/avionics`}>
                                 <Col xs={12}
-                                     className='HomeAvionics_titles global_cursor global_goldenrodtxt-btn Mechanics_col d-flex align-items-start justify-content-center p-0 m-0'>
+                                     className='HomeAvionics_titles global_cursor global_goldenrodtxt-btn Mechanics_col d-flex align-items-center justify-content-center p-0 m-0'>
                                     <Zoom>
                                         <p className='HomeAvionics_text-titles'> Pre-Buy Inspections</p>
                                     </Zoom>
                                 </Col>
                             </LinkContainer>
+
+                            <LinkContainer to={`/avionics`}>
+                                <Col xs={12}
+                                     className='HomeAvionics_titles global_cursor global_goldenrodtxt-btn Mechanics_col d-flex align-items-center justify-content-center p-0 m-0'>
+                                    <Zoom>
+                                        <p className='HomeAvionics_text-titles'>Annual Inspections</p>
+                                    </Zoom>
+                                </Col>
+                            </LinkContainer>
+
                         </Col>
 
                         {/*MOBILE IMAGE*/}
@@ -61,7 +71,7 @@ const HomeAvionics = () => {
                              className='HomeAvionics_panel-img HomeAvionics_img mx-auto my-0 py-0 d-sm-block d-md-none'> </Col>
 
 
-                        <Col lg={7} md={6} xs={12} className='mx-0 p-0 my-auto'>
+                        <Col lg={6} md={6} xs={12} className='mx-0 mt-auto'>
                             <Col xs={12} className='mb-2 pr-4 d-none d-md-block'>
                                 <Zoom>
                                     <h1 className='global_white'>You depend on your aircraft to carry you and your
@@ -69,7 +79,7 @@ const HomeAvionics = () => {
                                 </Zoom>
                             </Col>
 
-                            <Col xs={12} className='global_cursor mt-4 ml-2 text-center text-md-left'>
+                            <Col xs={12} className='global_cursor mt-4 text-center text-md-left'>
                                 <Zoom>
                                     <p className='pr-3 global_white'>
                                         Whether you need to add ADS-B OUT capabilities in order to meet the December
@@ -82,14 +92,13 @@ const HomeAvionics = () => {
                                 </Zoom>
                             </Col>
 
-                            <Col md={4} xs={12} className='global_cursor ml-lg-2 mb-5 pb-5 pt-1'>
+                            <Col xs={10} md={12} className='global_cursor mb-5 py-2 mx-auto d-flex justify-content-center global_dark-bg-btn'>
                                 <LinkContainer to={`/avionics`}>
-                                    <Button
-                                        className='global_dark-bg-btn d-flex justify-content-sm-center text-center'>
+                                    {/*<Button className=' text-center'>*/}
                                         <Zoom>
                                             Learn More
                                         </Zoom>
-                                    </Button>
+                                    {/*</Button>*/}
                                 </LinkContainer>
                             </Col>
 
@@ -102,10 +111,9 @@ const HomeAvionics = () => {
 
 
             </Row>
-            <
-            />
+        </>
 
-            )
-            };
+    )
+};
 
-            export default HomeAvionics
+export default HomeAvionics
