df8114ecc6dfda93790425371034ec2147863309
Shelby Polasek <shelbypolasek@gmail.com>
2021-06-11 13:45:23 -0500

    sticky top removed / padding btm adjusted

--- frontend/src/components/HeaderFooter/HeaderMobile.js
+++ frontend/src/components/HeaderFooter/HeaderMobile.js
@@ -29,7 +29,7 @@ const HeaderMobile = () => {
 
     return (
         <>
-            <header className='m-auto header d-block p-0 col-xs-12 sticky-top' style={{top: '0', height: '10vh'}}>
+            <header className='m-auto header d-block p-0 col-xs-12'>
                 <Navbar expand="xl"
                         collapseOnSelect
                         style={{backgroundColor: 'black'}}
@@ -49,7 +49,7 @@ const HeaderMobile = () => {
                     {/*  LOGO  */}
                     <LinkContainer className='col-xs-2' to='/'>
                         <img src={logo}
-                             className='HeaderMobile_logo'
+                             className='HeaderMobile_logo pb-1'
                              alt="Bison Aviation"
                         />
                     </LinkContainer>
