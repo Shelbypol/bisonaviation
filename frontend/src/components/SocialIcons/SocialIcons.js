5cd033dd962440ef5c3178a9a888c86986daa1d0
Shelby Polasek <shelbypolasek@gmail.com>
2021-07-28 13:49:28 -0500

    image height added to social icons w/ unsused code rmvd

--- frontend/src/components/SocialIcons/SocialIcons.js
+++ frontend/src/components/SocialIcons/SocialIcons.js
@@ -10,53 +10,53 @@ const SocialIcons = () => {
 
     return (
         <>
-                <Row className='SocialIcons_row mt-1 ml-1'>
-                    <Col xs={1}  className='m-0 p-0'>
-                        {/*  FACEBOOK */}
-                        <LinkContainer className='SocialIcons_col-bg' to='/'>
-                            <Nav.Link className='flip-container'>
-                                <div className='flipper'>
+            <Row className='SocialIcons_row' style={{border: '2px solid red'}}>
+                <Col className='m-0 p-0 d-flex justify-content-center' style={{border: '2px solid red'}}>
+                    {/*  FACEBOOK */}
+                    <LinkContainer className='SocialIcons_col-bg' to='/'>
+                        <Nav.Link className='flip-container'>
+                            <div className='flipper'>
                             <span className='SocialIcons_socialIcons front pt-1'>
                                 <i className="m-auto fab fa-facebook"> </i>
                             </span>
-                                    <span className='SocialIcons_socialIcons-size back pt-1'>
+                                <span className='SocialIcons_socialIcons-size back pt-1'>
                                 <i className="m-auto fab fa-facebook "> </i>
                             </span>
-                                </div>
-                            </Nav.Link>
-                        </LinkContainer>
-                    </Col>
-                    <Col xs={1}  className='mx-2 p-0 SocialIcons_mobile-col'>
-                        {/*  INSTAGRAM */}
-                        <LinkContainer className='SocialIcons_col-bg' to='/'>
-                            <Nav.Link className='flip-container'>
-                                <div className='flipper'>
+                            </div>
+                        </Nav.Link>
+                    </LinkContainer>
+                </Col>
+                <Col className='mx-2 p-0 d-flex justify-content-center'>
+                    {/*  INSTAGRAM */}
+                    <LinkContainer className='SocialIcons_col-bg' to='/'>
+                        <Nav.Link className='flip-container'>
+                            <div className='flipper'>
                             <span className='SocialIcons_socialIcons front pl-1 pt-1'>
                                 <i className="m-auto fab fa-instagram"> </i>
                             </span>
-                                    <span className='SocialIcons_socialIcons-size back pl-1 pt-1'>
+                                <span className='SocialIcons_socialIcons-size back pl-1 pt-1'>
                                 <i className="m-auto fab fa-instagram "> </i>
                             </span>
-                                </div>
-                            </Nav.Link>
-                        </LinkContainer>
-                    </Col>
-                    <Col xs={1} className='m-0 p-0 SocialIcons_mobile-col'>
-                        {/*  TWITTER */}
-                        <LinkContainer className='SocialIcons_col-bg' to='/'>
-                            <Nav.Link className='flip-container'>
-                                <div className='flipper'>
+                            </div>
+                        </Nav.Link>
+                    </LinkContainer>
+                </Col>
+                <Col className='m-0 p-0 d-flex justify-content-center'>
+                    {/*  TWITTER */}
+                    <LinkContainer className='SocialIcons_col-bg' to='/'>
+                        <Nav.Link className='flip-container'>
+                            <div className='flipper'>
                             <span className='SocialIcons_socialIcons front pt-1'>
                                 <i className="m-auto fab fa-twitter"> </i>
                             </span>
-                                    <span className='SocialIcons_socialIcons-size back pt-1'>
+                                <span className='SocialIcons_socialIcons-size back pt-1'>
                                 <i className="m-auto fab fa-twitter "> </i>
                             </span>
-                                </div>
-                            </Nav.Link>
-                        </LinkContainer>
-                    </Col>
-                </Row>
+                            </div>
+                        </Nav.Link>
+                    </LinkContainer>
+                </Col>
+            </Row>
         </>
     )
 };
